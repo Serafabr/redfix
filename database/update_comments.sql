36da9d974ce8089359b4584eb8c9e8199eab7ebc
hzlopes <hzlopes@gmail.com>
2021-02-18 14:40:25 -0300

    Rewrite query

--- database/update_comments.sql
+++ database/update_comments.sql
@@ -1,33 +1,36 @@
 with list_of_operations as (
   select
-    p.oid as poid,
-    d.objoid as doid,
+    d.objoid,
+    d.classoid,
+    d.objsubid,
     a.operation,
-    d.description,
     a.administrator,
     a.coordinator,
     a.supervisor,
     a.inspector,
     a.employee,
-    a.visitor
-  from pg_proc as p
-  inner join pg_namespace as n on (p.pronamespace = n.oid)
-  inner join pg_description as d on (d.objoid = p.oid)
-  inner join api_privileges as a on (a.operation = p.proname)
-  where n.nspname = 'api'
+    a.visitor,
+    d.description
+  from api_privileges as a
+  inner join pg_catalog.pg_proc as p on (a.operation = p.proname)
+  inner join pg_catalog.pg_description as d on (d.objoid = p.oid)
 )
--- select * from api_operations;
+-- select * from list_of_operations;
 update pg_description as pgd set description = (
-  select
+  select (
     l.description ||
-    E'\nExecute privilege granted to: \n' ||
+    E'\nGranted to: \n' ||
     case when l.administrator then E'* administrator\n' else '' end ||
     case when l.coordinator then E'* coordinator\n' else '' end ||
     case when l.supervisor then E'* supervisor\n' else '' end ||
     case when l.inspector then E'* inspector\n' else '' end ||
     case when l.employee then E'* employee\n' else '' end ||
     case when l.visitor then E'* visitor\n' else '' end
+  )
 )
 from list_of_operations as l
-where pgd.objoid = l.doid
+where
+  pgd.objoid = l.objoid and
+  pgd.classoid = l.classoid and
+  pgd.objsubid = l.objsubid
 ;
