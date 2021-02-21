9a5a1f0ba0031b51cd9df71f219add305f9710a4
hzlopes <hzlopes@gmail.com>
2021-02-21 20:26:17 -0300

    Create more functions for invoices api

---
+++ database/api/invoice/set_invoice_paid.sql
@@ -0,0 +1,21 @@
+\set function_name api.set_invoice_paid
+
+drop function if exists :function_name;
+create or replace function :function_name (
+  in "invoiceId" integer,
+  out id integer
+)
+  language plpgsql
+  as $$
+    begin
+      update invoices set paid = true where invoice_id = "invoiceId";
+      id = "invoiceId";
+    end;
+  $$
+;
+
+grant execute on function :function_name to coordinator, supervisor, inspector;
+
+select generate_api_documentation(:'function_name',E'Output `id`: the same as `invoiceId` input\n') as new_comment \gset
+
+comment on function :function_name is :'new_comment';
