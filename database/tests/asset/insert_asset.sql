83b0e5ff8c09af5a2ef6448151f39a98a77cc448
hzlopes <hzlopes@gmail.com>
2021-02-26 14:00:36 -0300

    Use literal instead of variable in insert_asset test

--- database/tests/asset/insert_asset.sql
+++ database/tests/asset/insert_asset.sql
@@ -1,6 +1,6 @@
 select api.insert_asset(
   'ELET-ET-' || substr(gen_random_uuid()::text,1,8),
   'Estações Transformadoras',
-  :'asset_category_electric'::integer,
+  2,
   102
 ) as new_asset_id, :mutation_ok + 1 as mutation_ok \gset
