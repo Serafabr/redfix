ec753e3c5cdd88102462af8ff1c2172a9ea28366
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-02 15:47:58 -0300

    Put drop icon on AvatarDropdown

--- frontend/src/components/Avatars/AvatarDropdown/AvatarDropdown.tsx
+++ frontend/src/components/Avatars/AvatarDropdown/AvatarDropdown.tsx
@@ -1,4 +1,5 @@
 import { Avatar } from '../Avatar/Avatar';
+import dropIcon from '../../../assets/icons/dropdown.svg';
 import style from './AvatarDropdown.module.scss';
 
 export const AvatarDropdown = () => {
@@ -9,6 +10,7 @@ export const AvatarDropdown = () => {
         <div className={style.Name}>Pedro Serafim</div>
         <div className={style.Job}>Engenheiro</div>
       </div>
+      <img className={style.DropIcon} src={dropIcon} alt="Dropdown"/>
     </div>
   )
 }
