8939fb89ea1f30507f69c501f778c98ace4e2e6b
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-04 13:12:34 -0300

    Add handleOnClick for the dropdown

--- frontend/src/components/Buttons/Dropdown/Dropdown.tsx
+++ frontend/src/components/Buttons/Dropdown/Dropdown.tsx
@@ -1,23 +1,32 @@
+import { useState } from 'react';
 import style from './Dropdown.module.scss';
 import ArrowDown from '../../../assets/icons/arrow-down.svg';
 
 export const Dropdown = () => {
+  const [ isOpen, setIsOpen ] = useState<boolean>(false);
+  
+  const handleOnClick = () => {
+    setIsOpen((prevState) => (!prevState));
+  };
+  
   return (
     <div className={style.Dropdown}>
-      <button className={style.DropdownButton}>
+      <button className={style.DropdownButton} onClick={handleOnClick}>
         <div>Ordens de Serviço</div>
         <div className={style.ButtonDownArrow}>
           <img src={ArrowDown} alt="Ícone dropdown"/>
         </div>
       </button>
-      <div className={style.ListWrapper}>
-        <li className={style.List}>
-          <ul className={style.Item}>Ordens de Serviço</ul>
-          <ul className={style.Item}>Equipamentos</ul>
-          <ul className={style.Item}>Edifícios</ul>
-          <ul className={style.Item}>Contratos</ul>
-        </li>
-      </div>
+      {isOpen && (
+        <div className={style.ListWrapper}>
+          <li className={style.List}>
+            <ul className={style.Item}>Ordens de Serviço</ul>
+            <ul className={style.Item}>Equipamentos</ul>
+            <ul className={style.Item}>Edifícios</ul>
+            <ul className={style.Item}>Contratos</ul>
+          </li>
+        </div>
+      )}
     </div>
   )
 }
