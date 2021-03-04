b710ee8c06a46e0b3fd2b2b103b84da54e09e7ea
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-04 14:13:57 -0300

    Create useOutsideHandler custom hook

---
+++ frontend/src/hooks/useOutsideHandler.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+type refProps = React.RefObject<HTMLInputElement>;
+type callbackProps = () => void;
+type eventProps = MouseEvent; // Type from the DOM
+
+export const useOutsideHandler = (ref: refProps, callback: callbackProps) => {
+  // Create a custom hook
+  useEffect(() => {
+    // Alert if clicked on outside an element
+    const handleClickOutside = (event: eventProps) => {
+      if (ref.current && ! ref.current.contains(event.target as HTMLElement)) {
+        callback();
+      }
+    }
+    
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    
+    // Unbind the event listener on clean up
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [ref])
+  
+}
