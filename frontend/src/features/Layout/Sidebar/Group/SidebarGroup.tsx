2cbbb46d14910d13ec1a7734d7a28e492fff3355
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-05 17:42:05 -0300

    Adjust layout on sidebar group

--- frontend/src/features/Layout/Sidebar/Group/SidebarGroup.tsx
+++ frontend/src/features/Layout/Sidebar/Group/SidebarGroup.tsx
@@ -1,5 +1,9 @@
 import style from './SidebarGroup.module.scss';
 
+import notesTask from '../../../../assets/icons/notes-tasks.svg';
+import wench from '../../../../assets/icons/tools-wench.svg';
+import idCard from '../../../../assets/icons/id-card.svg';
+
 export const SidebarGroup = () => {
   return (
     <div className={style.SidebarGroup}>
@@ -7,9 +11,24 @@ export const SidebarGroup = () => {
         Manutenções
       </div>
       <ul className={style.List}>
-        <li className={style.Item}>Tarefas</li>
-        <li className={style.Item}>Planos de Manutenção</li>
-        <li className={style.Item}>Monitor</li>
+        <li className={style.Item}>
+          <img src={notesTask} alt="Tarefas"/>
+          <div className={style.ItemText}>
+            Tarefas
+          </div>
+        </li>
+        <li className={style.Item}>
+          <img src={wench} alt="Planos de Manutenção"/>
+          <div className={style.ItemText}>
+            Planos de Manutenção
+          </div>
+        </li>
+        <li className={style.Item}>
+          <img src={idCard} alt="Planos de Manutenção"/>
+          <div className={style.ItemText}>
+            Monitor
+          </div>
+        </li>
       </ul>
     </div>
   )
