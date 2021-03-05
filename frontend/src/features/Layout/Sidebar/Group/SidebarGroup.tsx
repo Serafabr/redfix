ab1c9d20803e53627b19a1e4643e108412fb5c12
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-05 17:53:29 -0300

    Style sidebar icons with the color white

--- frontend/src/features/Layout/Sidebar/Group/SidebarGroup.tsx
+++ frontend/src/features/Layout/Sidebar/Group/SidebarGroup.tsx
@@ -1,8 +1,8 @@
 import style from './SidebarGroup.module.scss';
 
-import notesTask from '../../../../assets/icons/notes-tasks.svg';
-import wench from '../../../../assets/icons/tools-wench.svg';
-import idCard from '../../../../assets/icons/id-card.svg';
+import notesTask from '../../../../assets/icons/sidebar/notes-tasks.svg';
+import wench from '../../../../assets/icons/sidebar/tools-wench.svg';
+import idCard from '../../../../assets/icons/sidebar/id-card.svg';
 
 export const SidebarGroup = () => {
   return (
@@ -12,19 +12,25 @@ export const SidebarGroup = () => {
       </div>
       <ul className={style.List}>
         <li className={style.Item}>
-          <img src={notesTask} alt="Tarefas"/>
+          <div className={style.IconWrapper}>
+            <img src={notesTask} alt="Tarefas"/>
+          </div>
           <div className={style.ItemText}>
             Tarefas
           </div>
         </li>
         <li className={style.Item}>
-          <img src={wench} alt="Planos de Manutenção"/>
+          <div className={style.IconWrapper}>
+            <img src={wench} alt="Planos de Manutenção"/>
+          </div>
           <div className={style.ItemText}>
             Planos de Manutenção
           </div>
         </li>
         <li className={style.Item}>
-          <img src={idCard} alt="Planos de Manutenção"/>
+          <div className={style.IconWrapper}>
+            <img src={idCard} alt="Planos de Manutenção"/>
+          </div>
           <div className={style.ItemText}>
             Monitor
           </div>
