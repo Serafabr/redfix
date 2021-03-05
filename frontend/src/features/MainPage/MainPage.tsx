db49b6ba3025870d5f1756d1ae2be2c90102e951
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-05 11:43:57 -0300

    Create structure for MainPage

--- frontend/src/features/MainPage/MainPage.tsx
+++ frontend/src/features/MainPage/MainPage.tsx
@@ -1,7 +1,28 @@
+import { AppSidebarFooter } from '../Layout/AppSidebarFooter';
+import { AppSidebarNav } from '../Layout/AppSidebarNav';
+import style from './MainPage.module.scss';
+
 export const MainPage = () => {
   return (
-    <div>
-      
+    <div className={style.MainPage}>
+      <AppHeader />
+      <AppSidebar />
+      <main>
+        Hey
+      </main>
+      {/* <AppHeader>
+        <MainHeader />
+      </AppHeader>
+      <AppSideBar>
+        <AppSideBarHeader />
+        <AppSidebarNav />
+        <AppSidebarFooter />
+      </AppSideBar>
+      <main>
+        <Container>
+          MainBody
+        </Container>
+      </main> */}
     </div>
   )
 }
