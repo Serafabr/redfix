568c80677d03dd0f088bf05475f1e2ee005ff319
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-08 20:11:38 -0300

    Add hovered icon effect on sidebar item

--- frontend/src/features/Layout/Sidebar/Item/SidebarItem.tsx
+++ frontend/src/features/Layout/Sidebar/Item/SidebarItem.tsx
@@ -4,24 +4,30 @@ import style from './SidebarItem.module.scss';
 
 type Props = {
   label: string,
-  icon: string
+  icon: string,
+  hoveredIcon: string,
 }
 
 export const SidebarItem = ({
   label,
   icon,
+  hoveredIcon
 }: Props) => {
   
   const [isHovered, setIsHovered] = useState<boolean>(false);
   
   const toggleHover = () => {
-    
+    setIsHovered((prevState) => !prevState);
   }
   
   return (
-    <div  className={style.Item}>
+    <div 
+      className={style.Item} 
+      onMouseEnter={toggleHover} 
+      onMouseLeave={toggleHover}
+    >
       <div className={style.IconWrapper}>
-        <img src={icon} alt={label}/>
+        <img src={isHovered ? hoveredIcon : icon} alt={label}/>
       </div>
       <div className={style.ItemText}>
         {label}
