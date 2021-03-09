3d62d1492c6b88b654d6391de9994d87301e34b3
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-09 15:19:06 -0300

    Adjust padding on Divider

--- frontend/src/features/Layout/Sidebar/Group/SidebarGroup.tsx
+++ frontend/src/features/Layout/Sidebar/Group/SidebarGroup.tsx
@@ -1,8 +1,11 @@
 import style from './SidebarGroup.module.scss';
 
 import notesTask from '../../../../assets/icons/sidebar/notes-tasks.svg';
+import dualNotesTask from '../../../../assets/icons/sidebar/dualColor/notes-tasks-dual.svg';
 import wench from '../../../../assets/icons/sidebar/tools-wench.svg';
+import dualWench from '../../../../assets/icons/sidebar/dualColor/tools-wench-dual.svg';
 import idCard from '../../../../assets/icons/sidebar/id-card.svg';
+import dualIdCard from '../../../../assets/icons/sidebar/dualColor/id-card-dual.svg';
 import { Divider } from '../Divider/Divider';
 import { SidebarItem } from '../Item/SidebarItem';
 
@@ -12,13 +15,13 @@ export const SidebarGroup = () => {
       <Divider label="Manutenções" />
       <ul className={style.List}>
         <li>
-          <SidebarItem label="Tarefas" icon={notesTask} />
+          <SidebarItem label="Tarefas" icon={notesTask} hoveredIcon={dualNotesTask} />
         </li>
         <li>
-          <SidebarItem label="Planos de Manutenção" icon={wench} />
+          <SidebarItem label="Planos de Manutenção" icon={wench} hoveredIcon={dualWench} />
         </li>
         <li>
-          <SidebarItem label="Monitor" icon={idCard} />
+          <SidebarItem label="Monitor" icon={idCard} hoveredIcon={dualIdCard} />
         </li>
       </ul>
     </div>
