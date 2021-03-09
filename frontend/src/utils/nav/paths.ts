e31006bb9839b62faee32800e8fed3609f870eb7
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-09 16:12:17 -0300

    Create a path file

---
+++ frontend/src/utils/nav/paths.ts
@@ -0,0 +1,74 @@
+export const paths = {
+  assets: {
+    all:        '/ativos',
+    one:        '/ativos/:id',
+    create:     '/ativos/', // POST
+    createForm: '/ativos/formulário',
+    update:     '/ativos/:id', // PUT
+    updateForm: '/ativos/formulário/:id',
+  },
+  tasks: {
+    all:        '/tarefas',
+    one:        '/tarefas/:id',
+    create:     '/tarefas/', // POST
+    createForm: '/tarefas/formulário',
+    update:     '/tarefas/:id', // PUT
+    updateForm: '/tarefas/formulário/:id',
+  },
+  plans: {
+    all:        '/planos',
+    one:        '/planos/:id',
+    create:     '/planos/', // POST
+    createForm: '/planos/formulário',
+    update:     '/planos/:id', // PUT
+    updateForm: '/planos/formulário/:id',
+  },
+  monitors: {
+    all:        '/monitores',
+    one:        '/monitores/:id',
+    create:     '/monitores/', // POST
+    createForm: '/monitores/formulário',
+    update:     '/monitores/:id', // PUT
+    updateForm: '/monitores/formulário/:id',
+  },
+  invoices: {
+    all:        '/faturamentos',
+    one:        '/faturamentos/:id',
+    create:     '/faturamentos/', // POST
+    createForm: '/faturamentos/formulário',
+    update:     '/faturamentos/:id', // PUT
+    updateForm: '/faturamentos/formulário/:id',
+  },
+  depots: {
+    all:        '/depositos',
+    one:        '/depositos/:id',
+    create:     '/depositos/', // POST
+    createForm: '/depositos/formulário',
+    update:     '/depositos/:id', // PUT
+    updateForm: '/depositos/formulário/:id',
+  },
+  spec: {
+    all:        '/espectec',
+    one:        '/espectec/:id',
+    create:     '/espectec/', // POST
+    createForm: '/espectec/formulário',
+    update:     '/espectec/:id', // PUT
+    updateForm: '/espectec/formulário/:id',
+  },
+  team: {
+    all:        '/equipes',
+    one:        '/equipes/:id',
+    create:     '/equipes/', // POST
+    createForm: '/equipes/formulário',
+    update:     '/equipes/:id', // PUT
+    updateForm: '/equipes/formulário/:id',
+  },
+  person: {
+    all:        '/usuarios',
+    one:        '/usuarios/:id',
+    create:     '/usuarios/', // POST
+    createForm: '/usuarios/formulário',
+    update:     '/usuarios/:id', // PUT
+    updateForm: '/usuarios/formulário/:id',
+  },
+};
