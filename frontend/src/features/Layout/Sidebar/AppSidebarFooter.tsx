01a1cbcaad87c238319bc54b5e69bfde149cd78b
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-09 15:54:37 -0300

    Delete files that were not used on Sidebar folder

--- frontend/src/features/Layout/Sidebar/AppSidebarFooter.tsx
+++
@@ -1,7 +0,0 @@
-export const AppSidebarFooter = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- frontend/src/features/Layout/Sidebar/AppSidebarNav.tsx
+++
@@ -1,7 +0,0 @@
-export const AppSidebarNav = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
