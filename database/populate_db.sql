e3a8db718aeeb67b97147eb2543300895426abce
hzlopes <hzlopes@gmail.com>
2021-03-17 14:40:17 -0300

    Improve populate_db script

--- database/populate_db.sql
+++ database/populate_db.sql
@@ -1,39 +1,34 @@
--- set psql variables
-\set LAST_ERROR_MESSAGE ''
-\set LAST_ERROR_SQLSTATE '00000'
-\set ON_ERROR_STOP off
-\set QUIET on
-\set insert_energy_bills false
+rollback;
 
--- set pg_settings variables
+-- set QUIET on
+\set QUIET on
+
 -- "login" to allow inserts (mandatory person_id in some columns)
 set cookie.session.person_id to 1;
 
--- begin transaction;
+-- set ON_ERROR_STOP to on
+\set ON_ERROR_STOP on
+
+-- initialize string with comma separated list of all tested mutations
+\set all_mutations ''
+
+-- choose if energy_bills is to be populated
+\set insert_energy_bills false
+
+-- run mutations inside a transaction and commit
 begin transaction;
-
--- use api to insert sample data
 \i scripts/run_all_mutations.sql
-
--- energy bills
 \if :insert_energy_bills
   \i samples/energy_bills.sql
 \endif 
-
--- commit transaction
 commit transaction;
 
 -- set QUIET off
 \set QUIET off
 
 -- print message
-select :'LAST_ERROR_SQLSTATE' = '00000' as my_boolean_test \gset
-
-\if :my_boolean_test
-  \echo '\n\nThe database was populated\n\n'
-  \else
-    \echo '\n\nThere was an error\n\n'
-\endif
+\echo '\n\nThe database was populated\n\n'
 
 -- unset variables
 \i scripts/unset_psql_variables
+\set ON_ERROR_STOP off
