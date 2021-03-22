ec2c2b5b6d38df659647f5eb7b306a2942024053
hzlopes <hzlopes@gmail.com>
2021-03-22 11:00:21 -0300

    Create trigger to validate allocations boxes

---
+++ database/triggers/validate_allocation.sql
@@ -0,0 +1,19 @@
+\set trigger_name validate_allocation
+
+drop function if exists :trigger_name cascade;
+create or replace function :trigger_name ()
+  returns trigger
+  language plpgsql
+  as $$
+    begin
+      if (coalesce(new.internal_box_id, new.external_box_id) is not null)
+        then return new;
+        else raise exception '%', get_exception_message(401);
+      end if;
+    end;
+  $$
+;
+
+create trigger :trigger_name
+before insert or update on allocations
+for each row execute procedure :trigger_name();
