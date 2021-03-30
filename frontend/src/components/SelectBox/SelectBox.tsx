cb9c985b6d686831f6edf847961b996cae8c363f
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-30 20:00:12 -0300

    Adjust margin on scroll bar inside select box

--- frontend/src/components/SelectBox/SelectBox.tsx
+++ frontend/src/components/SelectBox/SelectBox.tsx
@@ -4,7 +4,7 @@ import blueCheckIcon from '../../assets/icons/blue-check.svg';
 
 export const SelectBox = () => {
   return (
-    <div>
+    <div className={style.SelectBox}>
       <div className={style.ListWrapper}>
         <li className={style.List}>
           <ul className={`${style.Item} ${style.Selected}`}><span className={style.TextItem}>Customizar tabela</span><img src={blueCheckIcon} alt="Selected" /></ul>
