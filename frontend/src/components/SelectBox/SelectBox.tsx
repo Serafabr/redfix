ff460356306fd797d4b0619128c399bdc1b61b52
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-30 19:31:21 -0300

    Style selected item on select box

--- frontend/src/components/SelectBox/SelectBox.tsx
+++ frontend/src/components/SelectBox/SelectBox.tsx
@@ -1,12 +1,14 @@
 import style from './SelectBox.module.scss';
 
+import blueCheckIcon from '../../assets/icons/blue-check.svg';
+
 export const SelectBox = () => {
   return (
     <div>
       <div className={style.ListWrapper}>
         <li className={style.List}>
-          <ul className={`${style.Item} ${style.Selected}`}>Customizar tabela</ul>
-          <ul className={`${style.Item} ${style.Selected}`}>Exportar para CSV</ul>
+          <ul className={`${style.Item} ${style.Selected}`}>Customizar tabelaaaaaaaaaaaaaaaaaa<img src={blueCheckIcon} alt="Selected" /></ul>
+          <ul className={`${style.Item} ${style.Selected}`}>Exportar para CSV<img src={blueCheckIcon} alt="Selected" /></ul>
           <ul className={style.Item}>Exportar para Excel</ul>
           <ul className={style.Item}>Exportar PDF</ul>
           <ul className={style.Item}>Customizar tabela</ul>
