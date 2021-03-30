bd3bfb03b07342009f43c20ade156be3190996d6
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-03-30 16:20:21 -0300

    Basic style for filter dropdown

--- frontend/src/components/Buttons/Filter/FilterDropdown.tsx
+++ frontend/src/components/Buttons/Filter/FilterDropdown.tsx
@@ -2,7 +2,7 @@ import style from './FilterDropdown.module.scss';
 
 export const FilterDropdown = () => {
   return (
-    <button>
+    <button className={style.Button}>
       Filter
     </button>
   )
