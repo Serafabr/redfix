000be0a6a64418561c6e0474f559db51d899162e
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-14 15:04:17 -0300

    Add close icon to the asset folder

--- frontend/src/components/Modals/Modal.tsx
+++ frontend/src/components/Modals/Modal.tsx
@@ -1,10 +1,21 @@
+import { Button, ButtonType } from '../Buttons';
 import style from './Modal.module.scss';
 
 export const Modal = () => {
   return (
     <div className={style.Modal}>
       <div className={style.ModalBox}>
-        Hey
+        <div className={style.ModalHeader}>
+          <span className={style.Title}>Editar progresso</span>
+        </div>
+        <div className={style.ModalContent}>
+          Teste
+        </div>
+        <div className={style.ModalFooter}>
+          <Button
+            text="Alterar progresso"
+          />
+        </div>
       </div>
     </div>
   )
