8ce5cf62d3ab67bd155b55793a768414b4596d05
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-15 16:27:03 -0300

    Detect when the mouse enter and leave (hover) the close button on the modal

--- frontend/src/components/Modals/Modal.tsx
+++ frontend/src/components/Modals/Modal.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonType } from '../Buttons';
 import style from './Modal.module.scss';
 import closeIcon from '../../assets/icons/modal/close.svg';
+import closeIconHovered from '../../assets/icons/modal/hovered/close.svg';
 
 export const Modal = () => {
   
@@ -8,13 +9,17 @@ export const Modal = () => {
     console.log("Fechar modal");
   };
   
+  const handleMouseEnter = () => {
+    console.log("Mouse Enter");
+  }
+  
   return (
     <div className={style.Modal}>
       <div className={style.Background}/>
       <div className={style.ModalBox}>
         <div className={style.ModalHeader}>
           <span className={style.Title}>Editar progresso</span>
-          <div className={style.IconWrapper}>
+          <div className={style.IconWrapper} onMouseEnter={() => {handleMouseEnter()}} onMouseLeave={() => {handleMouseEnter()}}>
             <img src={closeIcon} alt="Fechar"/>
           </div>
         </div>
