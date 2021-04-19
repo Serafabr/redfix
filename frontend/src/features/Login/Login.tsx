d2999650123a3c3fe1844138d1a8dc0a99dc3e58
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-19 19:23:48 -0300

    Add form (email and password input) on auth page

--- frontend/src/features/Login/Login.tsx
+++ frontend/src/features/Login/Login.tsx
@@ -7,6 +7,16 @@ export const Login = () => {
       <div className={style.Logo}>
         <img src={blueLogo} alt=""/>
       </div>
+      <div className={style.CardBox}>
+        <div className={style.Content}>
+          <form action="#">
+            <label htmlFor="email">E-mail</label>
+            <input type="text" placeholder="Digite seu e-mail..."/>
+            <label htmlFor="password">Senha</label>
+            <input type="text" placeholder="Digite sua senha..."/>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
