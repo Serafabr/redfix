e9aae66d008124996ec9071890044519fa633dad
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-20 18:23:15 -0300

    Adjust color on table header

--- frontend/src/components/Tables/Table.tsx
+++ frontend/src/components/Tables/Table.tsx
@@ -3,9 +3,16 @@ import style from './Table.module.scss';
 export const Table = () => {
   return (
     <table className={style.Table}>
+      <colgroup>
+        <col className={style.FirstCol} />
+        <col/>
+        <col/>
+        <col/>
+        <col/>
+      </colgroup>
       <thead className={style.Head}>
         <tr>
-          <th>Id</th>
+          <th >Id</th>
           <th>Título</th>
           <th>Status</th>
           <th>Localização</th>
@@ -14,49 +21,49 @@ export const Table = () => {
       </thead>
       <tbody className={style.Body}>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
           <td>31/12/2020</td>
         </tr>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
           <td>31/12/2020</td>
         </tr>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
           <td>31/12/2020</td>
         </tr>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
           <td>31/12/2020</td>
         </tr>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
           <td>31/12/2020</td>
         </tr>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
           <td>31/12/2020</td>
         </tr>
         <tr>
-          <td>0001</td>
+          <td >0001</td>
           <td>Manutenção no subsolo do Edifício Principal</td>
           <td>Concluído</td>
           <td>Edifício Principal</td>
