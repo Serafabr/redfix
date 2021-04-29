7e52b4ce677fb9f7e9eee8cdfe6ba46f23dfbdb0
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-29 15:04:56 -0300

    Create main routes

--- frontend/src/nav/routes/main.ts
+++ frontend/src/nav/routes/main.ts
@@ -2,17 +2,15 @@ import React from "react";
 import { paths } from '../paths';
 
 // MainPage
-import MainPage from "./views/MainPage";
+import MainPage from "../../features/MainPage";
 
+// Lazy imports
 // General pages
-const Dashboard = React.lazy(() => import("./views/Dashboard"));
-const NotFound = React.lazy(() => import("./views/Dashboard"));
+const Dashboard = React.lazy(() => import("../../features/Dashboard"));
 
 const routes = [
-  { path: paths.task.all, exact: true, name: "Ordens de serviços", component: Tasks, props: { mode: 'all' } },
-  { path: paths.task.one, exact: true, name: "Ordens de serviços", component: Task, props: { mode: 'one' } },
-  { path: paths.task.update, exact: true, name: "Ordens de serviços", component: TaskForm, props: { mode: 'update' } },
-  { path: paths.task.create, exact: true, name: "Ordens de serviços", component: TaskForm, props: { mode: 'create' } },
+  { path: paths.main.mainPage, exact: true, name: "Página Principal", component: MainPage },
+  { path: paths.main.dashboard, name: "Painel", component: Dashboard },
 ];
 
 export default routes;
