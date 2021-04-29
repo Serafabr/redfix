d3911badda04895808fe136ad5fae9a9a4f62d1d
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-29 17:05:15 -0300

    Create project routes

---
+++ frontend/src/nav/routes/projects.ts
@@ -0,0 +1,16 @@
+import React from "react";
+import { paths } from '../paths';
+
+// Lazy imports - Components
+const Projects = React.lazy(() => import("../../features/Projects"));
+const Project = React.lazy(() => import("../../features/Projects/Project"));
+const ProjectForm = React.lazy(() => import("../../features/Projects/ProjectForm"));
+
+const routes = [
+  { path: paths.projects.all, exact: true, name: "Tarefas", component: Projects },
+  { path: paths.projects.one, exact: true, name: "Tarefa", component: Project },
+  { path: paths.projects.createForm, exact: true, name: "Nova tarefa", component: ProjectForm },
+  { path: paths.projects.updateForm, exact: true, name: "Editar tarefa", component: ProjectForm },
+];
+
+export default routes;
