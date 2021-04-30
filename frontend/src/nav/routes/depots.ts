d0eb8f9376682aff2cedeb73b13cb8a1e6587605
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-04-30 12:26:39 -0300

    Create depot routes

---
+++ frontend/src/nav/routes/depots.ts
@@ -0,0 +1,16 @@
+import React from "react";
+import { paths } from '../paths';
+
+// Lazy imports - Components
+const Depots = React.lazy(() => import("../../features/Depots"));
+const Depot = React.lazy(() => import("../../features/Depots/Depot"));
+const DepotForm = React.lazy(() => import("../../features/Depots/DepotForm"));
+
+const routes = [
+  { path: paths.depots.all, exact: true, name: "Estoques", component: Depots },
+  { path: paths.depots.one, exact: true, name: "Estoque", component: Depot },
+  { path: paths.depots.createForm, exact: true, name: "Novo estoque", component: DepotForm },
+  { path: paths.depots.updateForm, exact: true, name: "Editar estoque", component: DepotForm },
+];
+
+export default routes;
