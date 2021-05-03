71d63bbe9c91a53e38f91c9725a0c04d0040d903
hzlopes <hzlopes@gmail.com>
2021-05-03 11:34:38 -0300

    Improve comments

--- database/roles/privileges.sql
+++ database/roles/privileges.sql
@@ -1,8 +1,10 @@
 -- alter owner of schema public
 alter schema public owner to administrator;
 
--- revoke all privileges from all roles
+-- revoke all privileges on the database from all roles
 revoke all on database :new_db_name from public, cmms_user, supervisor, inspector, employee, visitor;
+
+-- revoke all privileges on schemas from all roles
 revoke all on schema public from public, cmms_user, supervisor, inspector, employee, visitor;
 revoke all on schema api from public, cmms_user, supervisor, inspector, employee, visitor;
 revoke all on schema web from public, cmms_user, supervisor, inspector, employee, visitor;
@@ -22,13 +24,13 @@ grant usage on schema public to supervisor, inspector, employee, visitor;
 grant usage on schema api to supervisor, inspector, employee, visitor;
 grant usage on schema web to supervisor, inspector, employee, visitor;
 
--- grant privileges on objects in public schema
+-- set default privileges on objects in public schema
 alter default privileges in schema public grant select on tables to supervisor, inspector, employee, visitor;
 alter default privileges in schema public grant insert, update, delete on tables to supervisor, inspector, employee;
 alter default privileges in schema public grant usage on sequences to supervisor, inspector, employee;
 alter default privileges in schema public grant execute on routines to supervisor, inspector, employee, visitor;
 
--- grant privileges on objects in api schema
+-- set default privileges on objects in api schema
 -- * api schema only contains views and functions
 -- * execute privileges on routines in api schema are granted individually
 alter default privileges in schema api grant select on tables to supervisor, inspector, employee, visitor;
