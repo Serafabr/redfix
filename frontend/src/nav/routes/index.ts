7f92d26dbab2dbaaa85b2bd3f8d72c195aa7ded8
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-04 11:32:39 -0300

    Export all routes together (index file)

---
+++ frontend/src/nav/routes/index.ts
@@ -0,0 +1,31 @@
+import appliances from './appliances';
+import auth from './auth';
+import billings from './billings';
+import depots from './depots';
+import facilities from './facilities';
+import main from './main';
+import monitors from './monitors';
+import plans from './plans';
+import projects from './projects';
+import receipts from './receipts';
+import tasks from './tasks';
+import teams from './teams';
+import users from './users';
+
+const routes = [
+  ...appliances,
+  ...auth,
+  ...billings,
+  ...depots,
+  ...facilities,
+  ...main,
+  ...monitors,
+  ...plans,
+  ...projects,
+  ...receipts,
+  ...tasks,
+  ...teams,
+  ...users,
+];
+
+export default routes;
