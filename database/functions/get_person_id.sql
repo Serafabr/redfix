c66c066f3e5f7264ad7dbe77e83a7315c4abd15a
hzlopes <hzlopes@gmail.com>
2021-05-04 13:48:52 -0300

    Use optional boolean argument in current_setting

--- database/functions/get_person_id.sql
+++ database/functions/get_person_id.sql
@@ -7,7 +7,7 @@ create or replace function :function_name (
   language plpgsql
   as $$
     begin
-      select current_setting('cookie.session.person_id')::integer into personid;
+      select current_setting('cookie.session.person_id', true)::integer into personid;
       if (personid is null)
         then perform raise_exception(601);
       end if;
