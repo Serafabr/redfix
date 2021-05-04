b3400478cee0af391be8b1a52d021ff154797032
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-04 16:11:09 -0300

    Add suspense component on MainPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.scss';
 import { MainPage } from './features/MainPage/MainPage';
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <HashRouter>
+        <MainPage />
+      </HashRouter>
     </div>
   );
 }
--- frontend/src/features/MainPage/MainPage.tsx
+++ frontend/src/features/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { AppHeader, AppSidebar, AppSidebarHeader } from '../Layout';
 import { navItems } from '../../nav/navItems';
 import { Redirect, Route, Switch } from "react-router-dom";
@@ -18,20 +19,22 @@ export const MainPage = () => {
         <AppSidebar navItems={navItems} />
       </div>
       <main className={style.Body} style={{ backgroundColor: "#e5e5e5", padding: "24px 40px" }}>
-        <Switch>
-          {routes.map((route: RouteType, idx: number) => {
-            return route.component ? (
-              <Route
-                key={idx}
-                path={route.path}
-                exact={route.exact}
-                //name={route.name}
-                render={routerProps => <route.component {...routerProps} /*{...route.props}*/ />}
-              />
-            ) : null;
-          })}
-          <Redirect from="/" to={{ pathname: "/painel" }}/>
-        </Switch>
+        <Suspense fallback={() => (<div>Loading</div>)}>
+          <Switch>
+            {routes.map((route: RouteType, idx: number) => {
+              return route.component ? (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  exact={route.exact}
+                  //name={route.name}
+                  render={routerProps => <route.component {...routerProps} /*{...route.props}*/ />}
+                />
+              ) : null;
+            })}
+            <Redirect from="/" to={{ pathname: "/painel" }}/>
+          </Switch>
+        </Suspense>
       </main>
     </div>
   )
