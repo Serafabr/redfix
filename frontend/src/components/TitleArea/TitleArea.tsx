47356b327328f74fd050ff7002a77452db33bab8
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-05 15:43:38 -0300

    Create logic for TitleArea

--- frontend/src/components/TitleArea/TitleArea.tsx
+++ frontend/src/components/TitleArea/TitleArea.tsx
@@ -1,9 +1,21 @@
+import { Button, ButtonType } from '../Buttons';
+import { PageTitle } from '../PageTitle/PageTitle';
 import style from './TitleArea.module.scss';
+
+import { Plus as PlusIcon } from '../../components/Icons';
 
 export const TitleArea = () => {
   return (
-    <div>
-      
+    <div className={style.TitleArea}>
+      <PageTitle title="Tarefa" path="/tarefas" />
+      <div className={style.Buttons}>
+        <div className={style.ButtonWrapper}>
+          <Button text="Nova Tarefa" iconComponent={PlusIcon} />
+        </div>
+        <div className={style.ButtonWrapper}>
+          <Button buttonType={ButtonType.Secondary} justIcon iconComponent={PlusIcon} />
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/features/Tasks/Tasks.tsx
+++ frontend/src/features/Tasks/Tasks.tsx
@@ -1,10 +1,11 @@
 import { PageTitle } from '../../components/PageTitle/PageTitle';
+import { TitleArea } from '../../components/TitleArea/TitleArea';
 import style from './Task.module.scss';
 
 export const Tasks = () => {
   return (
     <div>
-      <PageTitle title="Tarefa" path="/tarefas" />
+      <TitleArea />
     </div>
   )
 }
