9339b74f34ca6a504ebc1d61ba71032a4acab3c4
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 18:35:55 -0300

    Create MoreHorizontal icon component

---
+++ frontend/src/components/Icons/MoreHorizontal.tsx
@@ -0,0 +1,28 @@
+import style from './Icons.module.scss';
+
+type Props = {
+  className?: string | null,
+}
+
+function SvgMoreHorizontal({ className = null }: Props) {
+  return (
+    <svg
+      width={16}
+      height={16}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${className || style.Icon}`}
+    >
+      <path
+        d="M8 8.667a.667.667 0 100-1.334.667.667 0 000 1.334zM12.667 8.667a.667.667 0 100-1.334.667.667 0 000 1.334zM3.333 8.667a.667.667 0 100-1.334.667.667 0 000 1.334z"
+        fill="#767676"
+        stroke="#767676"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default SvgMoreHorizontal;
--- frontend/src/components/Icons/index.ts
+++ frontend/src/components/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Plus } from './Plus';
 export { default as AlertCircle } from './AlertCircle';
 export { default as Search } from './Search';
+export { default as MoreHorizontal } from './MoreHorizontal'
