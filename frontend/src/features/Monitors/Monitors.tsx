90114d5b6373ceba84579c8b68a9984aca642fd2
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 15:34:14 -0300

    Add TitleArea on monitors

--- frontend/src/features/Monitors/Monitors.tsx
+++ frontend/src/features/Monitors/Monitors.tsx
@@ -1,9 +1,31 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Monitors.module.scss';
 
-export const Monitors = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+
+type MonitorsProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const monitorsButtons = [
+  <Button text="Novo monitor" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={PlusIcon} />
+];
+
+export const Monitors = ({
+  location
+}: MonitorsProps) => {
   return (
     <div>
-      Monitors
+      <TitleArea 
+        title="Monitores"
+        path={location.pathname}
+        buttons={monitorsButtons}
+      />
     </div>
   )
 }
