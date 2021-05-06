3507893e4cf38d09bbe01e5e5ce90b7abdda3795
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 19:20:00 -0300

    Add TitleArea on Facilities

--- frontend/src/features/Facilities/Facilities.tsx
+++ frontend/src/features/Facilities/Facilities.tsx
@@ -1,9 +1,32 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Facilities.module.scss';
 
-export const Facilities = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+import { MoreHorizontal as MoreIcon } from '../../components/Icons';
+
+type FacilitiesProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const facilitiesButtons = [
+  <Button text="Novo edifício" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={MoreIcon} />
+];
+
+export const Facilities = ({
+  location
+}: FacilitiesProps) => {
   return (
     <div>
-      Facilities
+      <TitleArea 
+        title="Edifícios"
+        path={location.pathname}
+        buttons={facilitiesButtons}
+      />
     </div>
   )
 }
