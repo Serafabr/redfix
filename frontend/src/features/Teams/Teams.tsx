b373cc72e65fd1b0ed752a03bc1998656e13d4cc
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 19:31:32 -0300

    Add TitleArea on Users and Teams

--- frontend/src/features/Teams/Teams.tsx
+++ frontend/src/features/Teams/Teams.tsx
@@ -1,9 +1,32 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Teams.module.scss';
 
-export const Teams = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+import { MoreHorizontal as MoreIcon } from '../../components/Icons';
+
+type TeamsProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const teamsButtons = [
+  <Button text="Nova equipe" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={MoreIcon} />
+];
+
+export const Teams = ({
+  location
+}: TeamsProps) => {
   return (
     <div>
-      Teams
+      <TitleArea 
+        title="Equipes"
+        path={location.pathname}
+        buttons={teamsButtons}
+      />
     </div>
   )
 }
--- frontend/src/features/Users/Users.tsx
+++ frontend/src/features/Users/Users.tsx
@@ -1,9 +1,32 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Users.module.scss';
 
-export const Users = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+import { MoreHorizontal as MoreIcon } from '../../components/Icons';
+
+type UsersProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const usersButtons = [
+  <Button text="Novo usuário" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={MoreIcon} />
+];
+
+export const Users = ({
+  location
+}: UsersProps) => {
   return (
     <div>
-      Users
+      <TitleArea 
+        title="Usuários"
+        path={location.pathname}
+        buttons={usersButtons}
+      />
     </div>
   )
 }
