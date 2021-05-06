5e86e24c80e3124e6cacc8894c7203be2b7924da
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 19:25:24 -0300

    Adjust width of buttons

--- frontend/src/features/Appliances/Appliances.tsx
+++ frontend/src/features/Appliances/Appliances.tsx
@@ -1,9 +1,32 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Appliances.module.scss';
 
-export const Appliances = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+import { MoreHorizontal as MoreIcon } from '../../components/Icons';
+
+type AppliancesProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const appliancesButtons = [
+  <Button text="Novo equipamento" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={MoreIcon} />
+];
+
+export const Appliances = ({
+  location
+}: AppliancesProps) => {
   return (
     <div>
-      Appliances
+      <TitleArea 
+        title="Equipamentos"
+        path={location.pathname}
+        buttons={appliancesButtons}
+      />
     </div>
   )
 }
