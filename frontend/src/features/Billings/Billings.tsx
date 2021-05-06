0d7293990552bf397b599b9877ea1e656e71600b
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 18:56:42 -0300

    Add TitleArea on Billings

--- frontend/src/features/Billings/Billings.tsx
+++ frontend/src/features/Billings/Billings.tsx
@@ -1,9 +1,32 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Billings.module.scss';
 
-export const Billings = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+import { MoreHorizontal as MoreIcon } from '../../components/Icons';
+
+type BillingsProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const billtingsButtons = [
+  <Button text="Nova fatura" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={MoreIcon} />
+];
+
+export const Billings = ({
+  location
+}: BillingsProps) => {
   return (
     <div>
-      Billings
+      <TitleArea 
+        title="Faturamentos"
+        path={location.pathname}
+        buttons={billtingsButtons}
+      />
     </div>
   )
 }
