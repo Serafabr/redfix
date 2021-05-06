0f674e5cb90dc1671b274cf5da2bbb00246fa7a0
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-06 19:27:27 -0300

    Add TitleArea on depots

--- frontend/src/features/Depots/Depots.tsx
+++ frontend/src/features/Depots/Depots.tsx
@@ -1,9 +1,33 @@
+import { TitleArea } from '../../components/TitleArea/TitleArea';
+import { Button, ButtonType } from '../../components/Buttons';
+
 import style from './Depots.module.scss';
 
-export const Depots = () => {
+import { Plus as PlusIcon } from '../../components/Icons';
+import { MoreHorizontal as MoreIcon } from '../../components/Icons';
+
+type DepotsProps = {
+  location: {
+    pathname: string
+  }
+};
+
+const depotsButtons = [
+  <Button text="Novo estoque" iconComponent={PlusIcon} />,
+  <Button buttonType={ButtonType.Secondary} justIcon iconComponent={MoreIcon} />
+];
+
+
+export const Depots = ({
+  location
+}: DepotsProps) => {
   return (
     <div>
-      Depots
+      <TitleArea 
+        title="Estoques"
+        path={location.pathname}
+        buttons={depotsButtons}
+      />
     </div>
   )
 }
