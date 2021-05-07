eaf65c945293d40bf5bf0244b271bdf7ad2a35df
hzlopes <hzlopes@gmail.com>
2021-05-07 10:38:13 -0300

    Refactor task_event validation function

--- database/triggers/validate_task_event.sql
+++ database/triggers/validate_task_event.sql
@@ -6,41 +6,42 @@ create or replace function :trigger_name ()
   language plpgsql
   as $$
     declare
+      task_current_team integer;
       is_event_ok boolean;
     begin
-
-      with last_send as (
-        select  t.task_id,
-                t.team_id
-          from tasks as t
-        where t.task_id = new.task_id
-      )
-      select  case new.task_event
-                when 'creation' then (true)
-                when 'modification' then (true)
-                when 'team' then (
-                  new.next_team_id is not null and
-                  (
-                    ls.team_id = new.team_id or
-                    current_role = 'administrator' or
-                    current_role = 'supervisor'
-                  )
-                )
-                when 'status' then (
-                  -- todo: check team_id of the person setting task status is equal the
-                  -- current team_id of the task; or check role is administrator or supervisor
-                  true
-                )
-                when 'note' then new.note is not null
-              end into is_event_ok
-        from last_send as ls
-      ;
-
+      select team_id into task_current_team from tasks where task_id = new.task_id;
+      select case new.task_event
+        when 'creation' then (true)
+        when 'modification' then (
+          task_current_team = new.team_id or
+          current_role = 'administrator' or
+          current_role = 'supervisor' or
+          current_role = 'inspector'
+        )
+        when 'team' then (
+          new.next_team_id is not null and (
+            task_current_team = new.team_id or
+            current_role = 'administrator' or
+            current_role = 'supervisor' or
+            current_role = 'inspector'
+          )
+        )
+        when 'status' then (
+          new.task_status_id is not null and (
+            task_current_team = new.team_id or
+            current_role = 'administrator' or
+            current_role = 'supervisor' or
+            current_role = 'inspector'
+          )
+        )
+        when 'note' then (
+          new.note is not null
+        )
+      end into is_event_ok;
       if is_event_ok
         then return new;
         else perform raise_exception(202);
       end if;
-
     end;
   $$
 ;
