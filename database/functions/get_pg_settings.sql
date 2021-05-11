a19a21d9ef2cf732ce48489d7ae713a4988039b6
Henrique Zaidan Lopes <hzlopes@senado.leg.br>
2021-05-11 08:22:56 -0300

    Improve function that shows PostgreSQL configurations to the client

--- database/functions/get_pg_settings.sql
+++ database/functions/get_pg_settings.sql
@@ -2,21 +2,22 @@
 
 drop function if exists :function_name;
 create or replace function :function_name (
-  out my_settings text
+  out my_settings jsonb
 )
   language plpgsql
   stable
   as $$
     begin
-      select
-        current_setting('application_name')::text || ',' ||
-        current_setting('client_encoding')::text || ',' ||
-        session_user::text || ',' ||
-        current_user::text || ',' ||
-        current_setting('transaction_read_only')::text || ',' ||
-        current_setting('statement_timeout')::text || ',' ||
-        current_setting('transaction_isolation')::text
-      into my_settings;
+      select jsonb_build_object(
+        'applicationName', current_setting('application_name')::text,
+        'clientEncoding', current_setting('client_encoding')::text,
+        'sessionUser', session_user::text,
+        'currentRole', current_role::text,
+        'personId', coalesce(current_setting('cookie.session.person_id',true),'_'),
+        'transactionReadOnly', current_setting('transaction_read_only')::text = 'on',
+        'transactionIsolation', current_setting('transaction_isolation')::text,
+        'statementTimeout', current_setting('statement_timeout')::numeric
+      ) into my_settings;
     end;
   $$
 ;
