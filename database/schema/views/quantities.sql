453b229b168b84a98fe13cbcd31ac12289994c6f
hzlopes <hzlopes@gmail.com>
2021-05-12 11:02:08 -0300

    Fix view to find supplies quantities in a depot

--- database/schema/views/quantities.sql
+++ database/schema/views/quantities.sql
@@ -1,30 +1,44 @@
 drop view if exists quantities cascade;
 create or replace view quantities as
   select
-    d.depot_id,
+    coalesce(i.depot_id,o.depot_id) as depot_id,
     coalesce(i.supply_id,o.supply_id) as supply_id,
-    coalesce(i.qty_allocated,0) - coalesce(o.qty_allocated,0) as qty_available,
-    coalesce(i.qty_proposed,0) - coalesce(o.qty_proposed,0) as qty_proposed,
-    coalesce(i.qty_approved,0) - coalesce(o.qty_approved,0) as qty_approved
-  from depots as d
-  inner join (
+    -- inputs
+    coalesce(i.total_inputs,0) as total_inputs,
+    coalesce(i.currently_proposed_inputs,0) as currently_proposed_inputs,
+    coalesce(i.currently_approved_inputs,0) as currently_approved_inputs,
+    coalesce(i.total_proposed_inputs,0) as total_proposed_inputs,
+    coalesce(i.total_approved_inputs,0) as total_approved_inputs,
+    -- outputs
+    coalesce(o.total_outputs,0) as total_outputs,
+    coalesce(o.currently_proposed_outputs,0) as currently_proposed_outputs,
+    coalesce(o.currently_approved_outputs,0) as currently_approved_outputs,
+    coalesce(o.total_proposed_outputs,0) as total_proposed_outputs,
+    coalesce(o.total_approved_outputs,0) as total_approved_outputs
+  from (
     select
-      a.target_depot_id,
-      a.supply_id,
-      sum(case when a.alloc_status_id = 1 then a.qty_allocated else 0 end) as qty_allocated,
-      sum(case when a.alloc_status_id = 2 then a.qty_proposed else 0 end) as qty_proposed,
-      sum(case when a.alloc_status_id = 3 then a.qty_approved else 0 end) as qty_approved
+      a.target_depot_id as depot_id,
+      a.supply_id as supply_id,
+      sum(case when a.alloc_status_id = 1 then a.qty_allocated else 0 end) as total_inputs,
+      sum(case when a.alloc_status_id = 2 then a.qty_proposed else 0 end) as currently_proposed_inputs,
+      sum(case when a.alloc_status_id = 3 then a.qty_approved else 0 end) as currently_approved_inputs,
+      sum(coalesce(a.qty_proposed,0)) as total_proposed_inputs,
+      sum(coalesce(a.qty_approved,0)) as total_approved_inputs
     from allocations as a
+    where a.target_depot_id is not null
     group by target_depot_id, supply_id
-  ) as i on (i.target_depot_id = d.depot_id)
-  inner join (
+  ) as i
+  full outer join (
     select
-      a.source_depot_id,
-      a.supply_id,
-      sum(case when a.alloc_status_id = 1 then a.qty_allocated else 0 end) as qty_allocated,
-      sum(case when a.alloc_status_id = 2 then a.qty_proposed else 0 end) as qty_proposed,
-      sum(case when a.alloc_status_id = 3 then a.qty_approved else 0 end) as qty_approved
+      a.source_depot_id as depot_id,
+      a.supply_id as supply_id,
+      sum(case when a.alloc_status_id = 1 then a.qty_allocated else 0 end) as total_outputs,
+      sum(case when a.alloc_status_id = 2 then a.qty_proposed else 0 end) as currently_proposed_outputs,
+      sum(case when a.alloc_status_id = 3 then a.qty_approved else 0 end) as currently_approved_outputs,
+      sum(coalesce(a.qty_proposed,0)) as total_proposed_outputs,
+      sum(coalesce(a.qty_approved,0)) as total_approved_outputs
     from allocations as a
+    where a.source_depot_id is not null
     group by source_depot_id, supply_id
-  ) as o on (o.source_depot_id = d.depot_id)
+  ) as o on (i.depot_id = o.depot_id and i.supply_id = o.supply_id)
 ;
