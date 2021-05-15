9afe6c03c1756605bc0e7f5d9f5d01720a8f3ee2
hzlopes <hzlopes@gmail.com>
2021-05-15 20:06:51 -0300

    Fix logging of logout route

--- backend/middlewares/morgan.js
+++ backend/middlewares/morgan.js
@@ -4,7 +4,10 @@ const path = require('path');
 const paths = require('../paths');
 
 morgan.token('separator', req => '-'.repeat(100));
-morgan.token('session', req => `${req.user.personId === '' ? '-' : req.user.personId}\t${req.user.role === '' ? '-' : req.user.role}`);
+morgan.token('session', req => {
+  const u = req.baseUrl !== paths.logout ? req.user : req.loggedOutUser;
+  return `${u.personId === '' ? '-' : u.personId}\t${u.role === '' ? '-' : u.role}`;
+});
 morgan.token('body', req => JSON.stringify(req.baseUrl === paths.login ? { username: req.body.username, password: '******' } : req.body));
 
 const templateForFile = `:date[iso]\t:remote-addr\t:method\t:url\t:status\t:response-time\t:session\t:body`;
--- backend/routes/logout.js
+++ backend/routes/logout.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.get(
   '/',
   (req, res) => {
+    req.loggedOutUser = req.user; // avoids error when logging (see morgan middleware)
     req.logout();
     req.session = null;
     res.status(401).end();
