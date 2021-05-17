d8eb93a01c4d4ea7746b94c2cb632a978f1c75ab
hzlopes <hzlopes@gmail.com>
2021-05-17 10:35:09 -0300

    Remove console.log

--- backend/middlewares/cmms-session.js
+++ backend/middlewares/cmms-session.js
@@ -1,6 +1,5 @@
 // Middleware to set personId and role of unauthenticated users
 module.exports =  (req, res, next) => {
   if(!req.session.populated) req.user = { personId: process.env.SESSION_DEFAULT_PERSON_ID, role: process.env.SESSION_DEFAULT_ROLE };
-  console.log(req.get('set-cookie'))
   req.user.role !== '' || req.baseUrl === '/' ? next() : res.status(401).end();
 }
