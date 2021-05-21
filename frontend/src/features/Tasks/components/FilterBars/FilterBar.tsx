937abced093e93b5ee050bbb5586417a61d690e6
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-21 16:01:43 -0300

    Refactor FilterBar

--- frontend/src/features/Tasks/components/FilterBars/FilterBar.tsx
+++ frontend/src/features/Tasks/components/FilterBars/FilterBar.tsx
@@ -1,34 +1,47 @@
+// Third party libraries
+import { useState } from 'react';
+// Components
 import { FilterButton, AlignListType } from '../../../../components/Buttons';
 import { FilterDropdown } from '../../../../components/Buttons';
-
-import style from './FilterBar.module.scss';
-
-import quickIcon from '../../../../assets/icons/quick.svg';
-import quickOrangeIcon from '../../../../assets/icons/quick-orange.svg';
-import bookmarkIcon from '../../../../assets/icons/bookmark.svg'
-import { handleOptionSelection } from './config/handleOptionSelection';
+// Utils
 import { 
   quickInitial,
   teamsInitial,
   statusInitial,
- } from './config/initialFilterStates';
+} from './config/initialFilterStates';
+// Icons
+import quickIcon from '../../../../assets/icons/quick.svg';
+import quickOrangeIcon from '../../../../assets/icons/quick-orange.svg';
+import bookmarkIcon from '../../../../assets/icons/bookmark.svg';
+import { handleOptionSelection } from './config/handleOptionSelection';
+// CSS
+import style from './FilterBar.module.scss';
 
- import { useState } from 'react';
+/*************************\
+ * PropTypes
+\*************************/
 
 type Props = {
   filterState: any,
 };
 
+/*************************\
+ * SelectBox Component
+\*************************/
+
 export const FilterBar = ({
   filterState,
 }: Props) => {
   
+  // State of all filters
   const [ quickFilter, setQuickFilter ] = useState(quickInitial);
   const [ teamsFilter, setTeamsFilter ] = useState(teamsInitial);
   const [ statusFilter, setStatusFilter ] = useState(statusInitial);
   
+  // Functions to handle item selection
   const [ handleOneItemSelection, handleManyItemsSelection ] = handleOptionSelection();
   
+  // Render
   return (
     <div>
       <div className={style.RightDivider}>
