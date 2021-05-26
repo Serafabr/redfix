b2855f48cae57c7b3e2f87f3a668be5c78d362f3
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-26 11:17:47 -0300

    Create pagination logic

--- frontend/src/components/Tables/Pagination/Pagination.tsx
+++ frontend/src/components/Tables/Pagination/Pagination.tsx
@@ -1,11 +1,25 @@
 import style from './Pagination.module.scss';
 
+import leftIcon from '../../../assets/icons/arrow-left.svg';
+import rightIcon from '../../../assets/icons/arrow-right.svg';
+
 const NUM_PAGE_BUTTONS_LARGE = 5;
 
-export const Pagination = () => {
-  const pages = 100;
-  const currentPage = 25;
+type Props = {
+  currentPage?: number
+}
+
+export const Pagination = ({
+  currentPage = 10,
+}) => {
+  const pages = 10;
   const setCurrentPage = (page: any) => {};
+  
+  if (currentPage > pages) {
+    currentPage = pages;
+  } else if (currentPage < 1) {
+    currentPage = 1;
+  }
   
   // const handleClickPagination = (pageClicked: string) => {
   //   switch (pageClicked) {
@@ -40,31 +54,41 @@ export const Pagination = () => {
   
   
   const maxPageButtons = NUM_PAGE_BUTTONS_LARGE;
-  const visiblePageNumbers: Array<number> = [currentPage];
+  const visiblePageNumbers: Array<number> = [];
   
   console.log("Pagination: ");
-  Array.from(Array(maxPageButtons - 1), (_, index) => index + 1).forEach((i) => {
+  Array.from(Array(maxPageButtons), (_, index) => index + 1).forEach((i) => {
+    console.log("ForEach: ", i);
     let page = currentPage + Number(i);
-    if (page > 1 && page < pages && visiblePageNumbers.length < maxPageButtons && !visiblePageNumbers.includes(page)) {
+    if (page > 1 && page < pages && visiblePageNumbers.length < maxPageButtons - 1 && !visiblePageNumbers.includes(page)) {
       visiblePageNumbers.push(page);
     }
 
     page = currentPage - Number(i);
-    if (page > 1 && page < pages && visiblePageNumbers.length < maxPageButtons && !visiblePageNumbers.includes(page)) {
+    if (page > 1 && page < pages && visiblePageNumbers.length < maxPageButtons - 1 && !visiblePageNumbers.includes(page)) {
       visiblePageNumbers.push(page);
     }
+    console.log(visiblePageNumbers);
   });
+  
+  console.log(visiblePageNumbers);
+  
+  if (currentPage !== 1 && currentPage !== pages) {
+    visiblePageNumbers.push(currentPage);
+  }
   
   const sortedPageNumbers = visiblePageNumbers.sort((a, b) => (a - b));
   
   const listPages = [1, ...sortedPageNumbers, pages];
+
+  console.log(listPages);
   
   const displayList: Array<string> = [];
   listPages.forEach((page, index) => {
     if (index === 1 && page !== 2) {
       displayList.push('...')
     }
-    if (index === listPages.length - 1 && page != pages - 1) {
+    if (index === listPages.length - 1 && listPages[listPages.length - 2] !== pages - 1) {
       displayList.push('...')
     }
     displayList.push(page.toString());
@@ -72,11 +96,17 @@ export const Pagination = () => {
   
   return (
     <ul className={style.Pagination}>
-      <li className={style.PaginationItem}>{'<'}</li>
-      {displayList.map((item) => (
-        <li className={`${style.PaginationItem} ${currentPage === Number(item) && style.Active}`}><a className={style.PaginationLink} href="#">{item}</a></li>
+      <li className={style.PaginationItem}>
+        <img src={leftIcon} alt="Anterior" />
+      </li>
+        {displayList.map((item) => (
+          <li className={`${style.PaginationItem} ${currentPage === Number(item) && style.Active}`}>
+            <a className={style.PaginationLink} href="#">{item}</a>
+          </li>
         ))}
-      <li className={style.PaginationItem}>{'>'}</li>
+      <li className={style.PaginationItem}>
+        <img src={rightIcon} alt="Próximo" />
+      </li>
     </ul>
   )
 }
