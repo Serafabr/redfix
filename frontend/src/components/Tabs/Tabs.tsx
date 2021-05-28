7baeeead269d84c111b0d4c77faf55205f373f50
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-28 11:25:06 -0300

    Add basic style to tabs

--- frontend/src/components/Tabs/Tabs.tsx
+++ frontend/src/components/Tabs/Tabs.tsx
@@ -12,12 +12,19 @@ const tabs = [
 
 export const Tabs = () => {
   return (
-    <div className={style.Tabs}>
-      <ul className={style.TabList}>
-        {tabs.map((tab) => (
-          <li className={style.TabItem}>{tab}</li>
-        ))}
-      </ul>
+    <div>
+      <div className={style.Tabs}>
+        <div className={style.TabsContainer}>
+          <ul className={style.TabList}>
+            {tabs.map((tab) => (
+              <li className={style.TabItem}>{tab}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div style={{ margin: "100px" }}>
+        TabePane
+      </div>
     </div>
   )
 }
