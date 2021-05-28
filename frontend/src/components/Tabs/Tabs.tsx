6c6998b44902c6eef6564ca21e509f76d09f6e08
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-05-28 14:07:39 -0300

    Get width on react for tabs container

--- frontend/src/components/Tabs/Tabs.tsx
+++ frontend/src/components/Tabs/Tabs.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import style from './Tabs.module.scss';
 
 const tabs = [
@@ -8,18 +9,33 @@ const tabs = [
   'Arquivos',
   'Faturamentos',
   'Monitoramentos',
+  'Monitoramentos',
+  'Monitoramentos',
 ];
 
+
 export const Tabs = () => {
+  
+  const tabContainerEl = useRef(null);
+  const tabsEl = useRef(null);
+  
+  console.log('tabContainerEl');
+  console.log(tabContainerEl.current && (tabContainerEl.current as any).offsetWidth);
+  console.log(tabsEl.current && (tabsEl.current as any).scrollWidth);
+  
   return (
-    <div>
-      <div className={style.Tabs}>
-        <div className={style.TabsContainer}>
+    <div>x
+      <div className={style.Tabs} ref={tabsEl}>
+        <div className={style.TabsContainer} ref={tabContainerEl}>
           <ul className={style.TabList}>
             {tabs.map((tab) => (
               <li className={style.TabItem}>{tab}</li>
             ))}
           </ul>
+        </div>
+        <div className={style.ButtonsContainer}>
+          <button>Button1</button>
+          <button>Button1</button>
         </div>
       </div>
       <div style={{ margin: "100px" }}>
