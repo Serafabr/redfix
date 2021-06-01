5db5c664e6888628ba110227e68767168a7411cd
hzlopes <hzlopes@gmail.com>
2021-06-01 14:34:33 -0300

    Rename variables

--- backend/cron/index.js
+++ backend/cron/index.js
@@ -5,7 +5,7 @@ import cron from 'cron';
 import { pgPool } from '../db/index.js';
 import paths from '../paths.js';
 
-const cronWritableStream = fs.createWriteStream(paths.cronLog, { flags: 'a' });
+const cronWriteStream = fs.createWriteStream(paths.cronLog, { flags: 'a' });
 
 const {
   PGDATABASE,
@@ -13,17 +13,17 @@ const {
   CRON_TIMEZONE,
   CRON_PATTERN_DUMP,
   CRON_PATTERN_DIFF,
-  CRON_PATTERN_REFRESH,
   CRON_PATTERN_DASHBOARD,
+  CRON_PATTERN_REFRESH,
 } = process.env;
 
 const dumpDatabase = async () => {
   try {
     const exec_ = promisify(exec);
     await exec_(`pg_dump -f ${paths.dump} -d ${PGDATABASE} -U ${PGADMINUSER} -w`);
-    cronWritableStream.write(`${(new Date()).toISOString()}\tdump\tok\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\tdump\tok\n`);
   } catch (error){
-    cronWritableStream.write(`${(new Date()).toISOString()}\tdump\t${error}\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\tdump\t${error}\n`);
   }
 }
 
@@ -32,30 +32,30 @@ const diffUploadedFiles = async () => {
     const UUIDs = fs.readdirSync(paths.files);
     const { rows: [ { dbUUIDs } ] } = await pgPool.query('select web.get_all_files_uuids() as "dbUUIDs"');
     const diffUUIDs = UUIDs.filter(uuid => (!dbUUIDs.includes(uuid)));
-    cronWritableStream.write(`${(new Date()).toISOString()}\tdiff\tok\n`);
-    const diffWritableStream = fs.createWriteStream(paths.diffLog);
-    diffWritableStream.write(`${diffUUIDs.join('\n')}\n`);
-    diffWritableStream.close();
+    cronWriteStream.write(`${(new Date()).toISOString()}\tdiff\tok\n`);
+    const diffWriteStream = fs.createWriteStream(paths.diffLog);
+    diffWriteStream.write(`${diffUUIDs.join('\n')}\n`);
+    diffWriteStream.close();
   } catch (error){
-    cronWritableStream.write(`${(new Date()).toISOString()}\tdiff\t${error}\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\tdiff\t${error}\n`);
   }
 }
 
 const updateDashboard = async () => {
   try {
     await pgPool.query('select web.update_dashboard()');
-    cronWritableStream.write(`${(new Date()).toISOString()}\tdashboard\tok\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\tdashboard\tok\n`);
   } catch (error) {
-    cronWritableStream.write(`${(new Date()).toISOString()}\tdashboard\t${error}\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\tdashboard\t${error}\n`);
   }
 }
 
 const refreshAllMVs = async () => {
   try {
     await pgPool.query('select web.refresh_all_materialized_views()');
-    cronWritableStream.write(`${(new Date()).toISOString()}\trefresh\tok\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\trefresh\tok\n`);
   } catch (error) {
-    cronWritableStream.write(`${(new Date()).toISOString()}\trefresh\t${error}\n`);
+    cronWriteStream.write(`${(new Date()).toISOString()}\trefresh\t${error}\n`);
   }
 }
 
@@ -73,7 +73,7 @@ const diff = new cron.CronJob({
   timezone: CRON_TIMEZONE,
 });
 
-const update = new cron.CronJob({
+const dashboard = new cron.CronJob({
   cronTime: CRON_PATTERN_DASHBOARD,
   onTick: updateDashboard,
   start: true,
@@ -90,6 +90,6 @@ const refresh = new cron.CronJob({
 export default {
   dump,
   diff,
-  update,
+  dashboard,
   refresh,
 }
