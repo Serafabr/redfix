3aa994923c682f705a07739c9c0c906ea6c10667
hzlopes <hzlopes@gmail.com>
2021-06-03 10:50:58 -0300

    Use try/catch blocks

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 import csvParse from 'csv-parse';
 import paths from '../paths.js';
 
-async function saveUploads(req, res, next){
+const saveUploads = (req, res, next) => {
   switch (req?.body?.variables?.uploadType){
     case 'files': saveFiles(req, res, next); break;
     case 'image': saveImage(req, res, next); break;
@@ -13,98 +13,90 @@ async function saveUploads(req, res, next){
     case 'energy': parseEnergyCsv(req, res, next); break;
     default: next();
   }
-}
+};
 
 // Helper function to write files to disk from a stream
-function saveLocal(stream, filePath){
-  return new Promise((resolve, reject) => stream
-    .on("error", error => {
-      if (stream.truncated){ fs.unlinkSync(filePath); }
-      reject(error);
-    })
-    .on("end", () => resolve())
-    .pipe(fs.createWriteStream(filePath))
-  );
-}
+const saveLocal = (stream, filePath) => new Promise((resolve, reject) => stream
+  .on("error", error => {
+    if (stream.truncated) fs.unlinkSync(filePath);
+    reject(error);
+  })
+  .on("end", () => resolve())
+  .pipe(fs.createWriteStream(filePath))
+);
 
-async function saveFiles(req, res, next){
-  const { files, filesMetadata } = req.body.variables;
-  Promise.all(files.map(async (file, i) => {
-    const resolvedFile = await file.promise;
-    const { filename, mimetype, encoding, createReadStream } = resolvedFile;
+const saveFiles = async (req, res, next) => {
+  try {
+    const { files, filesMetadata } = req.body.variables;
+    await Promise.all(files.map(async (file, i) => {
+      const resolvedFile = await file.promise;
+      const { filename, mimetype, encoding, createReadStream } = resolvedFile;
+      const stream = createReadStream();
+      const filePath = join(paths.files, filesMetadata[i].uuid);
+      return saveLocal(stream, filePath);
+    }))
+    next();
+  } catch (error){
+    console.log(error);
+    res.status(500).end();
+  }
+};
+
+const saveImage = async (req, res, next) => {
+  try {
+    const { image, imageMetadata } = req.body.variables;
+    const resolvedImage = await image.promise;
+    const { filename, mimetype, encoding, createReadStream } = resolvedImage;
     const stream = createReadStream();
-    const filePath = join(paths.files, filesMetadata[i].uuid);
-    return saveLocal(stream, filePath);
-  }))
-  .then(() => {
+    const imagePath = join(paths.images, imageMetadata.uuid);
+    await saveLocal(stream, imagePath);
     next();
-  })
-  .catch(error => {
-    console.log(error);
-  });
-}
-
-async function saveImage(req, res, next){
-  const { image, imageMetadata } = req.body.variables;
-  const resolvedImage = await image.promise;
-  const { filename, mimetype, encoding, createReadStream } = resolvedImage;
-  const stream = createReadStream();
-  const imagePath = join(paths.images, imageMetadata.uuid);
-  saveLocal(stream, imagePath)
-  .then(() => {
-    next();
-  })
-  .catch(error => {
+  } catch(error){
     console.log(error);
     res.status(500).end();
-  });
-}
+  }
+};
 
-async function saveAvatar(req, res, next){
-  const { avatar, avatarMetadata } = req.body.variables;
-  const resolvedAvatar = await avatar.promise;
-  const { filename, mimetype, encoding, createReadStream } = resolvedAvatar;
-  const stream = createReadStream();
-  const avatarPath = join(paths.images, avatarMetadata.uuid);
-  saveLocal(stream, avatarPath)
-  .then(() => {
+const saveAvatar = async (req, res, next) => {
+  try {
+    const { avatar, avatarMetadata } = req.body.variables;
+    const resolvedAvatar = await avatar.promise;
+    const { filename, mimetype, encoding, createReadStream } = resolvedAvatar;
+    const stream = createReadStream();
+    const avatarPath = join(paths.images, avatarMetadata.uuid);
+    await saveLocal(stream, avatarPath);
     next();
-  })
-  .catch(error => {
+  } catch (error){
     console.log(error);
     res.status(500).end();
-  });
-}
+  }
+};
 
-async function parseEnergyCsv(req, res, next){
-  const energyCsv = req.body.variables.energyCsv;
-  const resolvedEnergyCsv = await energyCsv.promise;
-  const { filename, mimetype, encoding, createReadStream } = resolvedEnergyCsv;
-  const stream = createReadStream();
-  const allValues = [];
-  stream
-  .pipe(csvParse({ from_line: 2, delimiter: ';' })) // skip header and set delimiter
-  .on("data", record => { // each record is a line of energy csv file
-    try {
+const parseEnergyCsv = async (req, res, next) => {
+  try {
+    const energyCsv = req.body.variables.energyCsv;
+    const resolvedEnergyCsv = await energyCsv.promise;
+    const { filename, mimetype, encoding, createReadStream } = resolvedEnergyCsv;
+    const stream = createReadStream();
+    const allValues = [];
+    stream
+    .pipe(csvParse({ from_line: 2, delimiter: ';' })) // skip header and set delimiter
+    .on("data", record => { // each record is a line of energy csv file
       // remove empty string at the end of the record
       record.pop();
       // generate string compatible with VALUES clause format of INSERT
       const values = `(${record.map(field => ("'" + field + "'")).join(',')})`;
       allValues.push(values);
-    } catch(error) {
-      console.log(error);
-      res.status(400).end();
-    }
-  })
-  .on("end", async () => {
-    req.body.variables.valuesString = allValues.join(',');
-    next();
-  })
-  .on("error", error => {
+    })
+    .on("end", async () => {
+      req.body.variables.valuesString = allValues.join(',');
+      next();
+    })
+  } catch (error){
     console.log(error);
-    res.status(400).end();
-  })
-}
+    res.status(500).end();
+  }
+};
 
 const router = Router();
 
