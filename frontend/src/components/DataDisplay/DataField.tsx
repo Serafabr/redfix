4d8824ed8776d213af468c89131274d3d5ce53f8
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-04 12:08:50 -0300

    DataField receives icon

--- frontend/src/components/DataDisplay/DataField.tsx
+++ frontend/src/components/DataDisplay/DataField.tsx
@@ -5,6 +5,7 @@ enum IconPositionT { Start, End };
 type DataFieldT = {
   label: string,
   data: string | number,
+  gridArea?: string,
   icon?: string,
   iconPosition?: IconPositionT
 };
@@ -12,15 +13,28 @@ type DataFieldT = {
 export const DataField = ({
   label,
   data,
+  gridArea,
   icon,
   iconPosition = IconPositionT.Start
 }: DataFieldT) => {
+  
+  const dataDisplayStyle = { gridArea };
+  
   return (
-    <div className={`${style.DataDisplay} ${style.DescriptionGrid}`}>
+    <div className={`${style.DataDisplay}`} style={dataDisplayStyle}>
       <div className={style.Label}>{label}</div>
       <div className={style.Data}>
-        <div className={style.Icon}>+</div>
+        {icon && iconPosition === IconPositionT.Start && (
+          <div className={style.Icon}>
+            <img src={icon} alt={label} />
+          </div>
+          )}
         <div className={style.Text}>{data}</div>
+        {icon && iconPosition === IconPositionT.End && (
+          <div className={style.Icon}>
+            <img src={icon} alt={label} />
+          </div>
+        )}
       </div>
     </div>
   )
