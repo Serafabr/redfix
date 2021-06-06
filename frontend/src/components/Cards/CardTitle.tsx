f75e3296e9eda16075c867fd9ca5e2d11c978fd6
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-06 14:04:29 -0300

    Add logic to CardTitle

--- frontend/src/components/Cards/CardTitle.tsx
+++ frontend/src/components/Cards/CardTitle.tsx
@@ -1,12 +1,33 @@
 import style from './Card.module.scss';
+import BookmarkIcon from '../../assets/icons/bookmark.svg';
+import BlueBookmarkIcon from '../../assets/icons/bookmark-checked.svg';
 
-export const CardTitle = () => {
+type CardTitleT = {
+  title: string,
+  withBookmark?: boolean,
+  bookmarkState?: boolean,
+  setBookmarkState?: (state: boolean) => void,
+}
+
+export const CardTitle = ({
+  title,
+  withBookmark = false,
+  bookmarkState = false,
+  setBookmarkState,
+}: CardTitleT) => {
+  
+  const handleBookmarkClick = () => {
+    setBookmarkState && setBookmarkState(!bookmarkState);
+  }
+  
   return (
     <div className={style.CardTitle}>
-      Manutenção no subsolo do Edifício Principal. Trocar todos os disjuntores e fazer revisão dos quadros elétricos.
-      <span className={style.CardBookmark}>
-        <img src={BookmarkIcon} alt="Favoritar tarefa" />
-      </span>
+      {title}
+      {withBookmark && (
+        <span className={style.CardBookmark} onClick={handleBookmarkClick}>
+          <img src={bookmarkState ? BlueBookmarkIcon : BookmarkIcon } alt="Favoritar tarefa" />
+        </span>
+      )}
     </div>
   )
 }
