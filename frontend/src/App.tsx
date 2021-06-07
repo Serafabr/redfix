2ad08a21c68334a274e33a0495bf1bfe09f7db3d
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-07 16:38:32 -0300

    Create scroll to top custom hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import { MainPage } from './features/MainPage/MainPage';
 import { HashRouter, Route, Switch } from "react-router-dom";
+import { ScrollToTop } from './components/ScrollToTop';
 
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
+        <ScrollToTop />
         <MainPage />
       </HashRouter>
     </div>
