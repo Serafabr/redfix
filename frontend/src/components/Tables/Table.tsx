7162c90a984eacecfcc078362f33b62c10ddce09
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-07 13:22:11 -0300

    Change fake data for Tasks

--- frontend/src/components/Tables/Table.tsx
+++ frontend/src/components/Tables/Table.tsx
@@ -23,7 +23,7 @@ export const Table = ({
       <colgroup>
         <col style={{ width: "10%" }}/>
         <col style={{ width: "40%" }}/>
-        <col style={{ width: "15%" }}/>
+        <col style={{ width: "15%", minWidth: "140px" }}/>
         <col style={{ width: "20%" }}/>
         <col style={{ width: "15%" }}/>
       </colgroup>
--- frontend/src/features/Tasks/components/TableStatus/TableStatus.tsx
+++ frontend/src/features/Tasks/components/TableStatus/TableStatus.tsx
@@ -1,5 +1,32 @@
 import style from './TableStatus.module.scss';
+
 import completedIcon from '../../../../assets/icons/status/completed.svg';
+import canceledIcon from '../../../../assets/icons/status/canceled.svg';
+import executingIcon from '../../../../assets/icons/status/executing.svg';
+import finishedIcon from '../../../../assets/icons/status/finished.svg';
+import pendingIcon from '../../../../assets/icons/status/pending.svg';
+
+enum StatusT { 
+  Waiting = 'Fila de espera', 
+  Pending = 'Pendente', 
+  Executing = 'Em execução', 
+  Suspended = 'Suspenso', 
+  Analysis = 'Em análise', 
+  Canceled = 'Cancelado', 
+  Finished = 'Finalizado', 
+  Completed = 'Concluído' 
+};
+
+const Status = {
+  [StatusT.Waiting]: { text: 'Fila de espera', icon: pendingIcon }, 
+  [StatusT.Pending]: { text: 'Pendente', icon: pendingIcon }, 
+  [StatusT.Executing]: { text: 'Em execução', icon: executingIcon }, 
+  [StatusT.Suspended]: { text: 'Suspenso', icon: canceledIcon }, 
+  [StatusT.Analysis]: { text: 'Em análise', icon: pendingIcon }, 
+  [StatusT.Canceled]: { text: 'Cancelado', icon: canceledIcon }, 
+  [StatusT.Finished]: { text: 'Finalizado', icon: finishedIcon }, 
+  [StatusT.Completed]: { text: 'Concluído', icon: completedIcon }
+}
 
 type TableStatusT = {
   value: string,
