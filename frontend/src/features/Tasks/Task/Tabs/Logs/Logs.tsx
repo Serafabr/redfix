62c35596fd0d7db9db734894e683f287826252e6
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-08 12:12:44 -0300

    Ellipse icons for logs

--- frontend/src/features/Tasks/Task/Tabs/Logs/Logs.tsx
+++ frontend/src/features/Tasks/Task/Tabs/Logs/Logs.tsx
@@ -1,8 +1,15 @@
 import style from './Logs.module.scss';
 
+import ellipseBlue from '../../../../../assets/icons/log/ellipse-blue.svg';
+import ellipseRed from '../../../../../assets/icons/log/ellipse-red.svg';
+import ellipseOrange from '../../../../../assets/icons/log/ellipse-orange.svg';
+
 export const Logs = () => {
   return (
     <div>
+      <img src={ellipseBlue} alt="" />
+      <img src={ellipseRed} alt="" />
+      <img src={ellipseOrange} alt="" />
       Histórico
     </div>
   )
