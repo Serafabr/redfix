b60d4122aa62eb8a8f48ece99091ad8a4d94d43a
hzlopes <hzlopes@gmail.com>
2021-06-08 14:18:23 -0300

    Create one more upload test

--- backend/tests/uploads.test.js
+++ backend/tests/uploads.test.js
@@ -7,7 +7,7 @@ describe('Uploads tests', () => {
 
   const url = 'http://localhost:3001/api';
 
-  test('Upload energy csv file', async () => {
+  test('Create energy bills / Upload energy csv file', async () => {
     let form = new FormData();
     form.append('operations', JSON.stringify({
       query: 'mutation ($valuesString: String!) { createEnergyBills(input: { valuesString: $valuesString }) { id } }',
@@ -21,6 +21,27 @@ describe('Uploads tests', () => {
     form.append('0', createReadStream(join(process.cwd(), 'tests', 'energy.csv')));
     const response = await got.post(url, { body: form, responseType: 'json' });
     expect(response.body.data.createEnergyBills).toMatchObject({ id: expect.any(Number) });
+  });
+
+  test.skip('Modify avatar / Upload image', async () => {
+    let form = new FormData();
+    form.append('operations', JSON.stringify({
+      query: 'mutation ($imageMetadata: FileInput!) { modifyAvatar(input: { avatarMetadata: $imageMetadata }) { id } }',
+      variables: {
+        uploadType: 'image',
+        image: null,
+        imageMetadata: {
+          uuid: `ffffffff-ffff-ffff-ffff-${Date.now().toString().substr(0,12)}`,
+          filename: 'myfile.jpeg',
+          size: 150,
+          mimetype: 'jpeg',
+        },
+      }
+    }));
+    form.append('map', JSON.stringify({0: ["variables.image"]}));
+    form.append('0', createReadStream(join(process.cwd(), 'public', 'images', 'test.jpeg')));
+    const response = await got.post(url, { body: form, responseType: 'json' });
+    expect(response.body.data.modifyAvatar).toMatchObject({ id: expect.any(Number) });
   });
 
 });
