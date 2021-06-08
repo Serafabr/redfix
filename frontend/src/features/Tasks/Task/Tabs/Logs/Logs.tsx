8be176f3db92070ab799a86b405c82dffa7aeccc
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-08 14:48:09 -0300

    Style log item

--- frontend/src/features/Tasks/Task/Tabs/Logs/Logs.tsx
+++ frontend/src/features/Tasks/Task/Tabs/Logs/Logs.tsx
@@ -28,11 +28,47 @@ export const Logs = () => {
       </div>
       <div className={style.LogsWrapper}>
         <div className={style.Log}>
-          <img src={ellipseBlue} alt="Mensagem" />
-          <div className={style.SystemMessage}>Pedro Serafim escreveu:</div>
-          <div className={style.UserMessage}> Aguardando a chegada dos materiais.</div>
-          <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+          <img className={style.Icon} src={ellipseBlue} alt="Mensagem" />
+          <div className={style.MessageBox}>
+          <div className={style.SystemMessage}><span className={style.Highlight}>Pedro Serafim</span> escreveu:</div>
+            <div className={style.UserMessage}> Aguardando a chegada dos materiais.</div>
+            <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+          </div>
         </div>
+        <div className={style.Log}>
+          <img className={style.Icon} src={ellipseBlue} alt="Mensagem" />
+          <div className={style.MessageBox}>
+            <div className={style.SystemMessage}><span className={style.Highlight}>Pedro Serafim</span> escreveu:</div>
+            <div className={style.UserMessage}> Aguardando a chegada dos materiais.</div>
+            <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+          </div>
+        </div>
+        <div className={style.Log}>
+          <img className={style.Icon} src={ellipseBlue} alt="Mensagem" />
+          <div className={style.MessageBox}>
+          <div className={style.SystemMessage}><span className={style.Highlight}>Pedro Serafim</span> escreveu:</div>
+            <div className={style.UserMessage}> Segue a tarefa após o ajuste de todos os materiais.</div>
+            <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+          </div>
+        </div>
+        <div className={style.Log}>
+          <img className={style.Icon} src={ellipseRed} alt="Mensagem" />
+          <div className={style.MessageBox}>
+          <div className={style.SystemMessage}><span className={style.Highlight}>Pedro Serafim</span> tramitou a tarefa da <span className={style.Highlight}>Coemant</span> para <span className={style.Highlight}>Seplag</span>.</div>
+            <div className={style.UserMessage}> Aguardando a chegada dos materiais.</div>
+            <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+          </div>
+        </div>
+        <div className={style.Log}>
+          <img className={style.Icon} src={ellipseOrange} alt="Mensagem" />
+          <div className={style.MessageBox}>
+          <div className={style.SystemMessage}><span className={style.Highlight}>Pedro Serafim</span> alterou o status de <span className={style.Highlight}>Em análise</span> para <span className={style.Highlight}>Em execução</span>.</div>
+            <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+          </div>
+        </div>
+        <button className={style.MoreButton}>
+          Mostrar mais...
+        </button>
       </div>
     </div>
   )
