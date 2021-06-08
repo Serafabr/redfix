b6c2c1b0d2e21c9d64874ebb856442042a48d81c
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-08 13:50:47 -0300

    Create log container for messages

--- frontend/src/features/Tasks/Task/Tabs/Logs/Logs.tsx
+++ frontend/src/features/Tasks/Task/Tabs/Logs/Logs.tsx
@@ -3,14 +3,37 @@ import style from './Logs.module.scss';
 import ellipseBlue from '../../../../../assets/icons/log/ellipse-blue.svg';
 import ellipseRed from '../../../../../assets/icons/log/ellipse-red.svg';
 import ellipseOrange from '../../../../../assets/icons/log/ellipse-orange.svg';
+import ellipseGray from '../../../../../assets/icons/log/ellipse-gray.svg';
 
 export const Logs = () => {
   return (
     <div>
-      <img src={ellipseBlue} alt="" />
-      <img src={ellipseRed} alt="" />
-      <img src={ellipseOrange} alt="" />
-      Histórico
+      <div className={style.FilterBar}>
+        <button className={style.LogButton}>
+          <img src={ellipseBlue} alt="Nova mensagem" />
+          Mensagens
+        </button>
+        <button className={style.LogButton}>
+          <img src={ellipseOrange} alt="Nova mensagem" />
+          Alterações de Status
+        </button>
+        <button className={style.LogButton}>
+          <img src={ellipseRed} alt="Nova mensagem" />
+          Tramitações
+        </button>
+        <button className={`${style.LogButton} ${style.Inactive}`}>
+          <img src={ellipseGray} alt="Nova mensagem" />
+          Tramitações
+        </button>
+      </div>
+      <div className={style.LogsWrapper}>
+        <div className={style.Log}>
+          <img src={ellipseBlue} alt="Mensagem" />
+          <div className={style.SystemMessage}>Pedro Serafim escreveu:</div>
+          <div className={style.UserMessage}> Aguardando a chegada dos materiais.</div>
+          <div className={style.LogTime}>18/01/2021 às 02:01:28 pm</div>
+        </div>
+      </div>
     </div>
   )
 }
