451993e25cf751bfdf3ddf873546754885bda779
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-14 15:47:03 -0300

    Adjust tableConfig for Billings

--- frontend/src/features/Tasks/Task/Tabs/tabConfig.tsx
+++ frontend/src/features/Tasks/Task/Tabs/tabConfig.tsx
@@ -1,6 +1,7 @@
 import { Logs, NewMessage } from './Logs';
 import { Supplies, AddSupply, AcceptSupply } from './Supplies';
 import { Facilities, AddFacility } from './Facilities';
+import { Billings, AddBilling } from './Billings';
 
 export const tabConfig: any = {
   logs: { 
@@ -18,8 +19,12 @@ export const tabConfig: any = {
     view: <Facilities />, 
     buttons:[<AddFacility />] 
   },
+  billings: { 
+    name: "Faturamentos", 
+    view: <Billings />, 
+    buttons: [<AddBilling/>],
+  },
   files: { name: "Arquivos", view: <div>Arquivos</div>, buttons:[] },
-  billings: { name: "Faturamentos", view: <div>Faturamentos</div>, buttons:[] },
   monitors: { name: "Monitores", view: <div>Monitores</div>, buttons:[] },
   // checklist: { name: "Checklist", view: <div>Checklist</div>, buttons:[] },
 }
