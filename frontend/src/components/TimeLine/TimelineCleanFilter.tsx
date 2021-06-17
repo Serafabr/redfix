f9cd68f9807fc5156a99acdb1af2360f679c9da1
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-17 11:08:26 -0300

    Style CleanFilter button for Timeline

--- frontend/src/components/TimeLine/TimelineCleanFilter.tsx
+++ frontend/src/components/TimeLine/TimelineCleanFilter.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import style from './Timeline.module.scss';
+import closeIcon from '../../assets/icons/x-circle.svg';
 
 export const TimelineCleanFilter = () => {
   return (
-    <div>
+    <div className={style.CleanFilter}>
       Limpar filtros
+      <img src={closeIcon} alt="Limpar" />
     </div>
   )
 }
