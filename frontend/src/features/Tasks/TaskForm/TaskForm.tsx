2911efb3d433ee1117190a44146ae57762663221
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-17 15:38:04 -0300

    Add general style for task form

--- frontend/src/features/Tasks/TaskForm/TaskForm.tsx
+++ frontend/src/features/Tasks/TaskForm/TaskForm.tsx
@@ -8,6 +8,9 @@ import { CardHeader } from '../../../components/Cards/CardHeader';
 import { SingleCardHeader } from '../../../components/Cards/SingleCard';
 import { SingleCardContent } from '../../../components/Cards/SingleCard';
 
+import { Badge } from '../../../components/Badges';
+import { ColorType } from '../../../components/Badges/Badge';
+
 export const TaskForm = () => {
   
   const location = useLocation<string>();
@@ -23,9 +26,40 @@ export const TaskForm = () => {
         <CardHeader>
           <SingleCardHeader>
             <CardTitle
-              title="Informações gerais"
-              badges={[]}
+              title="Informações gerais sobre a tarefa"
+              badges={[
+                <Badge text="Formulário incompleto" color={ColorType.Orange} />,
+              ]}
             />
+            <Badge text="Etapa 01 de 05" color={ColorType.Blue} />
+          </SingleCardHeader>
+        </CardHeader>
+        <SingleCardContent>
+          Hello
+        </SingleCardContent>
+        <CardHeader>
+          <SingleCardHeader>
+            <CardTitle
+              title="Datas e prazos para execução"
+              badges={[
+                <Badge text="Preenchido sem erros" color={ColorType.Green} />,
+              ]}
+            />
+            <Badge text="Etapa 02 de 05" color={ColorType.Blue} />
+          </SingleCardHeader>
+        </CardHeader>
+        <SingleCardContent>
+          Hello
+        </SingleCardContent>
+        <CardHeader>
+          <SingleCardHeader>
+            <CardTitle
+              title="Ativos relacionados à tarefa"
+              badges={[
+                <Badge text="Preenchido com erros" color={ColorType.Red} />,
+              ]}
+            />
+            <Badge text="Etapa 03 de 05" color={ColorType.Blue} />
           </SingleCardHeader>
         </CardHeader>
         <SingleCardContent>
