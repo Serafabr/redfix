97ea70f87adb2ef41323ddbaeb9657c5d1bca6bf
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-17 15:17:44 -0300

    Add card on task form

--- frontend/src/features/Tasks/TaskForm/TaskForm.tsx
+++ frontend/src/features/Tasks/TaskForm/TaskForm.tsx
@@ -1,9 +1,37 @@
 import style from './TaskForm.module.scss';
+import { TitleArea } from '../../../components/TitleArea/TitleArea';
+import { useLocation, useParams } from 'react-router';
+
+import { Card } from '../../../components/Cards';
+import { CardTitle } from '../../../components/Cards/CardTitle';
+import { CardHeader } from '../../../components/Cards/CardHeader';
+import { SingleCardHeader } from '../../../components/Cards/SingleCard';
+import { SingleCardContent } from '../../../components/Cards/SingleCard';
 
 export const TaskForm = () => {
+  
+  const location = useLocation<string>();
+  
   return (
     <div>
-      Formulário
+      <TitleArea 
+        title={`Cadastrar tarefa`}
+        path={location.pathname}
+        buttons={[]}
+      />
+      <Card>
+        <CardHeader>
+          <SingleCardHeader>
+            <CardTitle
+              title="Informações gerais"
+              badges={[]}
+            />
+          </SingleCardHeader>
+        </CardHeader>
+        <SingleCardContent>
+          Hello
+        </SingleCardContent>
+      </Card>
     </div>
   )
 }
