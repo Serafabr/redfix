76a78daaf17889ebed44e52d48f521da9857cd90
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-18 16:30:29 -0300

    Avatar receives image from props

--- frontend/src/components/Avatars/Avatar/Avatar.tsx
+++ frontend/src/components/Avatars/Avatar/Avatar.tsx
@@ -1,10 +1,14 @@
-import avatar from '../../../assets/avatar/avatar.png';
+import defaultAvatar from '../../../assets/avatar/avatar.png';
 import style from './Avatar.module.scss';
 
-type Props = any;
+type Props = {
+  avatar?: string,
+};
 
 // Avatar image
-export const Avatar = (props: Props) => {
+export const Avatar = ({
+  avatar = defaultAvatar
+}: Props) => {
   return (
     <div className={style.Avatar}>
       <img src={avatar} alt="Avatar do usuário"/>
