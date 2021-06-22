519fd7d53350e97fabaf11be24d762d67f7f32ff
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-22 15:24:09 -0300

    Fix index on Cards

--- frontend/src/components/Cards/index.ts
+++ frontend/src/components/Cards/index.ts
@@ -1,1 +1,6 @@
 export { Card } from './Card/Card';
+export { CardTitle } from './CardTitle/CardTitle';
+export { SingleCardContent } from './SingleCard/SingleCardContent';
+export { SingleCardHeader } from './SingleCard/SingleCardHeader';
+export { TableCardButtons } from './TableCard/TableCardButtons';
+export { TableCardContent } from './TableCard/TableCardContent';
