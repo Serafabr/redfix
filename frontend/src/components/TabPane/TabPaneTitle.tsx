43bfd0b66a007343a98546b1ad61cface76e65c0
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-24 14:03:38 -0300

    Refactor TabPane component

--- frontend/src/components/TabPane/TabPaneTitle.tsx
+++ frontend/src/components/TabPane/TabPaneTitle.tsx
@@ -1,8 +1,16 @@
 import style from './TabPane.module.scss';
 
+/*************************\
+ * PropTypes
+\*************************/
+
 type TabPaneTitleT = {
   title: string,
 }
+
+/*************************\
+ * TabPane Component
+\*************************/
 
 export const TabPaneTitle = ({
   title,
