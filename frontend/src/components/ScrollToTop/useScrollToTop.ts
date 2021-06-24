bd9d82d5116e5b7aa718aa474b8d066fb9a43e48
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-06-24 13:26:15 -0300

    Refactor ScrollToTop

--- frontend/src/components/ScrollToTop/useScrollToTop.ts
+++ frontend/src/components/ScrollToTop/useScrollToTop.ts
@@ -1,18 +1,11 @@
 import { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+/*************************\
+ * Custom Hook
+\*************************/
 
 export const useScrollToTop = (scrollPage: any) => {
-  // const history = useHistory();
-  
-  // useEffect(() => {
-  //   console.log("Mounting scroll listener...")
-  //   const unlisten = history.listen(() => {
-  //     console.log("Applying scroll");
-  //     window.scrollTo(0, 0);
-  //   });
-    
-  //   return () => { unlisten() };
-  // }, []);
   const { pathname } = useLocation();
   
   useEffect(() => {
