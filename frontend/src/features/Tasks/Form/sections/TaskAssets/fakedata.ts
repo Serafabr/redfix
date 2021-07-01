ae7da90b499192862d73ebfee742eb10bf6e881f
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-01 19:04:34 -0300

    Create fake data for TaskAssets on Form

---
+++ frontend/src/features/Tasks/Form/sections/TaskAssets/fakedata.ts
@@ -0,0 +1,17 @@
+export const dataAssets = [
+  {
+    id: 'CASF-AX1-061',
+    asset: 'Edifício Principal - Anexo 01',
+    category: 'Sistema de endereçamento',
+  },
+  {
+    id: 'EDPR-MEZ-467',
+    facility: 'Edifício Principal',
+    category: 'Sistema de endereçamento',
+  },
+  {
+    id: 'ELET-0459294',
+    facility: 'Nobreak do grupo motor gerador 01',
+    parent: 'Sistema elétrico',
+  },
+];
