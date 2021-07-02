f5e62d1ca8330bf2237cbd8e1bff4401dafb11a7
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-02 14:08:14 -0300

    Create tableConfig for TaskFiles

---
+++ frontend/src/features/Tasks/Form/sections/TaskFiles/tableConfig.tsx
@@ -0,0 +1,46 @@
+import { CellFormat } from '../../../../../components/Tables/Cells/CellFormat';
+import { MoreButton } from '../../../../../components/Tables';
+
+type ColumnStyle = {
+  [id: string]: any,
+}
+
+export const columnsAssets = [
+  {
+    Header: 'Data de inclusão',
+    id: 'date',
+    accessor: (row: any) => row.date, // accessor is the "key" in the data
+    Cell: CellFormat({ isCenter: true, isBold: true })
+  },
+  {
+    Header: 'Arquivo',
+    id: 'file',
+    accessor: (row: any) => row.file,
+  },
+  {
+    Header: 'Criado por',
+    id: 'createdBy',
+    accessor: (row: any) => row.createdBy,
+    Cell: CellFormat({ isCenter: true }),
+  },
+  {
+    id: 'Tamanho',
+    Header: 'size',
+    accessor: (row: any) => row.size,
+    Cell: CellFormat({ isCenter: true }),
+  },
+  {
+    id: 'moreButton',
+    Header: '',
+    accessor: (row: any) => row.id,
+    Cell: MoreButton,
+  },
+];
+
+export const columnStyle: ColumnStyle = {
+  date: { width: '150px', textAlign: 'center' },
+  file: { width: '50%' },
+  createdBy: { width: '30%', textAlign: 'center' },
+  size: { width: '20%', textAlign: 'center' },
+  moreButton: { width: '40px', textAlign: 'center' },
+};
