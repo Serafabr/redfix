30580b65ca48e4c3a44f97e550ba24ce416d2c91
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-04 18:55:51 -0300

    Create default options for dropdown, and add dropdown on AddAssetModal

---
+++ frontend/src/components/Inputs/Dropdown/defaultOptions.ts
@@ -0,0 +1,9 @@
+export const defaultOptions = {
+  option1: { name: 'Opção 01' },
+  option6: { name: 'Opção 02' },
+  option2: { name: 'Opção 03' },
+  option3: { name: 'Opção 04' },
+  option4: { name: 'Opção 05' },
+  option5: { name: 'Opção 06' },
+  option7: { name: 'Opção 07' },
+}
--- frontend/src/features/Tasks/Form/sections/TaskAssets/AddAssetModal.tsx
+++ frontend/src/features/Tasks/Form/sections/TaskAssets/AddAssetModal.tsx
@@ -1,11 +1,21 @@
 // Components
 import { Button } from '../../../../../components/Buttons'
-import { Modal } from '../../../../../components/Modals'
+import { Modal } from '../../../../../components/Modals';
+import { InputField } from '../../../../../components/Inputs';
+import { Dropdown } from '../../../../../components/Inputs';
+// Helper functions
+import { handleOneItemSelection } from '../../../../../components/SelectBox';
+import { defaultOptions } from '../../../../../components/Inputs/Dropdown/defaultOptions';
 // Style
 import style from './TaskAssets.module.scss';
+import { useState } from 'react';
 
 
 export const AddAssetModal = () => {
+  
+  const [ asset, setAsset ] = useState<Array<string>>([]);
+  const assetOptions = defaultOptions;
+  
   return (
     <Modal
       modalBoxClasses={style.ModalBox}
@@ -14,7 +24,20 @@ export const AddAssetModal = () => {
       setIsOpened={()=>{}}
       buttons={[<Button text="Adicionar"/>]}
     >
-      Adicionar um novo ativo
+      <InputField
+        label="Categoria"
+        gridArea="category"
+        error={false}
+        errorMessage={"Valor incorreto!"}
+      >
+        <Dropdown 
+          options={assetOptions}
+          selectionArray={asset}
+          onSelectItem={handleOneItemSelection(setAsset)}
+          boxWidth={250}
+          sortItems={true}
+        />
+      </InputField>
     </Modal>
   )
 }
