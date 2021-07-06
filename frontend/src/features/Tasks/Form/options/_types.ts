dde826fdbe0a248ed08495b2ce1e8e3199950a62
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-06 16:43:26 -0300

    Create types for task form options

--- frontend/src/features/Tasks/Form/options/_types.ts
+++ frontend/src/features/Tasks/Form/options/_types.ts
@@ -12,3 +12,34 @@ export type TeamOptions = {
     nodes: Array<Team>,
   },
 }
+
+/*************************\
+ * Project category type
+\*************************/
+
+export type Project = {
+  name: string,
+  projectId: number,
+}
+
+export type ProjectOptions = {
+  projects: {
+    nodes: Array<Project>,
+  },
+}
+
+/*************************\
+ * Asset category type
+\*************************/
+
+export type Asset = {
+  name: string,
+  assetId: number,
+  assetSf: string,
+}
+
+export type AssetOptions = {
+  assets: {
+    nodes: Array<Asset>,
+  },
+}
