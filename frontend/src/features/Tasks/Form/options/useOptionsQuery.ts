168ac5ee80e9433a41760a0b79ee35fe0a434836
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-06 16:55:02 -0300

    Complete useOptionsQuery with all options for task form

--- frontend/src/features/Tasks/Form/options/useOptionsQuery.ts
+++ frontend/src/features/Tasks/Form/options/useOptionsQuery.ts
@@ -2,29 +2,43 @@ import { useMemo } from 'react';
 import { useQuery } from '@apollo/client'
 import { OptionsType } from '../../../../components/SelectBox/_types';
 // Graphql
-import { TEAM_OPTIONS } from './graphql';
-import { prepareTeams } from './prepareOptions';
+import { TEAM_OPTIONS, PROJECT_OPTIONS, ASSET_OPTIONS } from './graphql';
+import { prepareTeams, prepareProjects, prepareAssets } from './prepareOptions';
 import { TeamOptions } from './_types';
 
 /*************************\
- * Custom hook - Get options to task form
+ * Custom hook - Get options for task form
 \*************************/
 
 export const useOptionsQuery = () => {
   
-  // Team options
-  const { loading: loadingTeam, error: errorTeam, data: dataTeam } = useQuery(TEAM_OPTIONS);
+  // Query Options
+  const { loading: loadingTeams, error: errorTeams, data: dataTeams } = useQuery(TEAM_OPTIONS);
+  const { loading: loadingProjects, error: errorProjects, data: dataProjects } = useQuery(PROJECT_OPTIONS);
+  const { loading: loadingAssets, error: errorAssets, data: dataAssets } = useQuery(ASSET_OPTIONS);
   
-  // Prepare team options
+  // Prepare team options 
   const dataTeamOptions = useMemo(() => {
-    if (loadingTeam || errorTeam) { return undefined; }
-    return prepareTeams(dataTeam);
-  }, [dataTeam]);
+    if (loadingTeams || errorTeams) { return undefined; }
+    return prepareTeams(dataTeams);
+  }, [dataTeams, loadingTeams, errorTeams]);
+  
+  const dataProjectOptions = useMemo(() => {
+    if (loadingProjects || errorProjects) { return undefined; }
+    return prepareProjects(dataProjects);
+  }, [dataProjects, loadingProjects, errorProjects]);
+  
+  const dataAssetOptions = useMemo(() => {
+    if (loadingAssets || errorAssets) { return undefined; }
+    return prepareAssets(dataAssets);
+  }, [dataAssets, loadingAssets, errorAssets]);
   
   
-  // result
-  const teamOptions = { loading: loadingTeam, error: errorTeam, data: dataTeamOptions }
+  // Result
+  const teamOptions = { loading: loadingTeams, error: errorTeams, data: dataTeamOptions }
+  const projectOptions = { loading: loadingProjects, error: errorProjects, data: dataProjectOptions }
+  const assetOptions = { loading: loadingAssets, error: errorAssets, data: dataAssetOptions }
   
   // Return the result
-  return { teamOptions };  
+  return { teamOptions, projectOptions, assetOptions };  
 }
