0867bd9b50c62e825178252309b9871eb8f616b9
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-06 16:47:42 -0300

    Create prepareOptions functions for options on task form

--- frontend/src/features/Tasks/Form/options/prepareOptions.ts
+++ frontend/src/features/Tasks/Form/options/prepareOptions.ts
@@ -1,6 +1,6 @@
 // Types
 import { OptionsType } from '../../../../components/SelectBox/_types';
-import { Team, TeamOptions } from './_types';
+import { Team, TeamOptions, Project, ProjectOptions, Asset, AssetOptions } from './_types';
 
 /*************************\
  * Prepare team options
@@ -18,6 +18,48 @@ export const prepareTeams = (teamOptions: TeamOptions) => {
   nodes.forEach((team: Team) => {
     const { teamId, name } = team;
     options[teamId] = { name }
+  });
+  
+  return options;
+}
+
+/*************************\
+ * Prepare project options
+\*************************/
+
+export const prepareProjects = (projectOptions: ProjectOptions) => {
+  
+  // Get array of teams
+  const { projects } = projectOptions;
+  const { nodes } = projects;
+  
+  // Result
+  const options: OptionsType = {};
+  
+  nodes.forEach((project: Project) => {
+    const { projectId, name } = project;
+    options[projectId] = { name }
+  });
+  
+  return options;
+}
+
+/*************************\
+ * Prepare asset options
+\*************************/
+
+export const prepareAssets = (assetOptions: AssetOptions) => {
+  
+  // Get array of teams
+  const { assets } = assetOptions;
+  const { nodes } = assets;
+  
+  // Result
+  const options: OptionsType = {};
+  
+  nodes.forEach((asset: Asset) => {
+    const { assetId, assetSf, name } = asset;
+    options[assetId] = { name: `${assetSf} - ${name}` }
   });
   
   return options;
