fdea15cbc70de81d9b10f7851366fec832b1aa4a
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-08 17:01:44 -0300

    Create createTask mutation

---
+++ frontend/src/features/Tasks/Form/mutation/useCreateTask.ts
@@ -0,0 +1,6 @@
+import { useMutation } from '@apollo/client'
+import { CREATE_TASK } from './createTask';
+
+export const useCreateTask = () => {
+  const [ createTask, { data } ] = useMutation(CREATE_TASK);
+}
