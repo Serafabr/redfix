9964785018f6e3a50622a6264b94ec616a547c7b
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-12 13:23:39 -0300

    Fix timeline problems (capitalization)

--- frontend/src/components/Timeline/_types.ts
+++
@@ -1,1 +0,0 @@
-export enum IconColorType { Red, Blue, Orange }; 
--- frontend/src/components/Timeline/index.ts
+++
@@ -1,6 +0,0 @@
-// Item
-export { TimelineItem } from './TimelineItem/TimelineItem';
-// Butons
-export { TimelineCleanFilter } from './TimelineButtons/TimelineCleanFilter';
-export { TimelineFilter } from './TimelineButtons/TimelineFilter';
-export { TimelineButtonContainer } from './TimelineButtons/TimelineButtonContainer';
