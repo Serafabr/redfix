5e6afd494fc30df85efe4b92389acc477f7e95c5
Pedro Henrique Serafim <serafaobr@gmail.com>
2021-07-13 14:42:41 -0300

    Style cancel create task modal

--- frontend/src/features/Tasks/Form/modals/CancelCreateTask/CancelCreateTask.tsx
+++ frontend/src/features/Tasks/Form/modals/CancelCreateTask/CancelCreateTask.tsx
@@ -1,5 +1,8 @@
+import { Button } from '../../../../../components/Buttons';
 import { Modal } from '../../../../../components/Modals';
 import style from './CancelCreateTask.module.scss';
+
+import { ButtonType } from '../../../../../components/Buttons/_types';
 
 export const CancelCreateTask = () => {
   return (
@@ -7,8 +10,18 @@ export const CancelCreateTask = () => {
       isOpened={true}
       setIsOpened={()=>{}}
       title="Cancelar criação"
+      buttons={[
+        <Button text="Voltar" buttonType={ButtonType.Warning} onClick={()=>{}} />,
+        <Button text="Cancelar" buttonType={ButtonType.Danger} onClick={()=>{}} />,
+      ]}
+      modalBoxClasses={style.ModalBox}
     >
-      Hey
+      <div className={style.Title}>
+        Você tem certeza que deseja cancelar?
+      </div>
+      <div className={style.Subtitle}>
+        Ao cancelar a criação de uma tarefa, todas as informações preenchidas serão permanentemente deletadas.
+      </div>
     </Modal>
   )
 }
